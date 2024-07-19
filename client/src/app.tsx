6882b89115128d68e9bebaf69d6e2786776c0666
dalramjwi <dalramjwi@gmail.com>
2024-07-19 16:30:08 +0900

    Feat|버튼 이벤트 할당

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Input from "./components/Input";
 import Button from "./components/Button";
+import useDataServeEvent from "./hooks/useDataServeEvent";
 const App: React.FC = () => {
   return (
     <div>
       <Input />
-      <Button />
+      <Button eventFunc={useDataServeEvent} />
     </div>
   );
 };
