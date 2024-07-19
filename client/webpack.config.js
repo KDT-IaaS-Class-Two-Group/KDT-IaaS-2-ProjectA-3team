c92cabacd64e2db8edd2f78879744a56f1880a60
naviadev <qo2oq1020@gmail.com>
2024-07-19 17:04:06 +0900

    Config | webpack.config.js : alias를 통해 빠른 디렉토리 접근이 가능하도록 구문을 수정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,8 +2,16 @@ const path = require('path');
 
 module.exports = {
   entry: "./dist/index.js",
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.bundle.js"
-  }
+  },
+  resolve: {
+    alias: {
+      '@validate': path.resolve(__dirname, 'dist/model/auth/validate/'),
+      '@request': path.resolve(__dirname, 'dist/model/auth/request')
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
 }
