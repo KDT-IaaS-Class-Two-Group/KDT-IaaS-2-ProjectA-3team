9730e7bb7915c48e76496c530e0f16e97617173c
신지윤 <ranji12@naver.com>
2024-07-19 15:45:13 +0900

    render 작성

--- client/dist/index.js
+++ client/dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
-var _reactDom = _interopRequireDefault(require("react-dom"));
 var _app = _interopRequireDefault(require("./app"));
+var _react = _interopRequireDefault(require("react"));
+var _client = _interopRequireDefault(require("react-dom/client"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(_app["default"], null), document.getElementById('root'));
+var root = _client["default"].createRoot(document.getElementById('root'));
+root.render( /*#__PURE__*/_react["default"].createElement(_app["default"], null));
