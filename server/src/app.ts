fe982bc01c07a48c96be6df7ccfebc6ba9ecb4d2
dalramjwi <dalramjwi@gmail.com>
2024-07-19 15:58:58 +0900

    Feat|cors 모듈 추가 및 사용

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import path from 'path';
-
+import express from "express";
+import * as dotenv from "dotenv";
+import path from "path";
+const cors = require("cors");
 dotenv.config({ path: `${__dirname}/../../.env` });
 
 const port = process.env.PORT;
 const app = express();
 
-app.use(express.static(`${process.cwd()}/../client/dist`))
+app.use(cors());
+app.use(express.static(`${process.cwd()}/../client/dist`));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/../client/dist/index.html");
 });
 
