94ce62016cd15ec0d07b4f37baa8c0a1dff4e6a6
dalramjwi <dalramjwi@gmail.com>
2024-07-19 15:34:31 +0900

    Feat|Butto 태그 생성

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,10 +2,14 @@ import React from "react";
 const Input: React.FC = () => {
   return <input type="text" placeholder="Enter your input here" />;
 };
+const Button: React.FC = () => {
+  return <button>Submit</button>;
+};
 const App: React.FC = () => {
   return (
     <div>
       <Input />
+      <Button />
     </div>
   );
 };
