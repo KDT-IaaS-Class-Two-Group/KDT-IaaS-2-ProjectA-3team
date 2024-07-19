e2761895a7fa994f1b986c923e1592a6509882a1
신지윤 <ranji12@naver.com>
2024-07-19 15:45:45 +0900

    client부분 작성

--- client/dist/app.js
+++ client/dist/app.js
@@ -1,12 +1,55 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var _react = _interopRequireWildcard(require("react"));
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 var App = function App() {
-  return /*#__PURE__*/_react["default"].createElement("div", null, "Hello, React!");
+  var _useState = (0, _react.useState)(''),
+    _useState2 = _slicedToArray(_useState, 2),
+    content = _useState2[0],
+    newContent = _useState2[1];
+  var _useState3 = (0, _react.useState)(''),
+    _useState4 = _slicedToArray(_useState3, 2),
+    innerContent = _useState4[0],
+    newInnerContent = _useState4[1];
+  var send = function send() {
+    var data = {
+      content: content
+    };
+    fetch("http://localhost:3001/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return newInnerContent(innerContent + data.content);
+    });
+  };
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("input", {
+    type: "text",
+    value: content,
+    onChange: function onChange(element) {
+      return newContent(element.target.value);
+    },
+    placeholder: "\uB0B4\uC6A9\uC744 \uC785\uB825\uD558\uC138\uC694"
+  }), /*#__PURE__*/_react["default"].createElement("button", {
+    onClick: send
+  }, "\uC804\uC1A1\uD558\uAE30"), /*#__PURE__*/_react["default"].createElement("div", {
+    id: "inner"
+  }, innerContent));
 };
 var _default = exports["default"] = App;
--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const App: React.FC = () => {
-  return <div>Hello, React!</div>;
-};
+const App = () =>{
+  const [content, newContent] = useState('');
+  const [innerContent, newInnerContent] = useState('');
+  const send=()=>{
+    const data = {
+      content:content
+    }
+    fetch("http://localhost:3001/send",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(data)
+    })
+    .then(response=>response.json())
+    .then(data => newInnerContent(innerContent+data.content))
+  }
+  return(
+  <div>
+    <input type="text" value={content} onChange={(element)=>newContent(element.target.value)}placeholder="내용을 입력하세요"/>
+    <button  onClick={send}>전송하기</button>
+    <div id="inner">{innerContent}</div>
+  </div>
+  )
+}
 
 export default App;
