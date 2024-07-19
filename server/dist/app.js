21f89ee78fb64e15afffb79b33bd8309eb3aac8f
신지윤 <ranji12@naver.com>
2024-07-19 17:59:58 +0900

    db연결 후 insert 성공

--- server/dist/app.js
+++ server/dist/app.js
@@ -22,6 +22,42 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -29,22 +65,49 @@ exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var dotenv = __importStar(require("dotenv"));
 var path_1 = __importDefault(require("path"));
-var body_parser_1 = __importDefault(require("body-parser"));
+var pg_1 = require("pg");
+var pool = new pg_1.Pool({
+    user: "postgres",
+    host: 'localhost',
+    database: 'shindatabase',
+    password: '1234',
+    port: 5432
+});
 dotenv.config({ path: "".concat(__dirname, "/../../.env") });
 var port = process.env.PORT;
 var app = (0, express_1["default"])();
-app.use(body_parser_1["default"].json()); // JSON 형식의 요청 본문을 파싱하기 위해 body-parser 미들웨어 사용
-// 정적 파일 서비스 설정
+app.use(express_1["default"].json()); //!w 집에서 확인
 app.use(express_1["default"].static(path_1["default"].join(__dirname, '../../client/dist')));
-app.get('/', function (req, res) {
-    res.sendFile(path_1["default"].join(__dirname, '../../client/dist/index.html'));
-});
 // /send 경로에 대한 POST 요청 처리
-app.post('/send', function (req, res) {
-    var content = req.body.content;
-    var a = res.json({ content: content });
-    console.log(a);
-});
+app.post('/send', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var content, client, err_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                content = req.body.content;
+                res.json({ content: content });
+                return [4 /*yield*/, pool.connect()];
+            case 1:
+                client = _a.sent();
+                _a.label = 2;
+            case 2:
+                _a.trys.push([2, 4, 5, 6]);
+                return [4 /*yield*/, client.query('INSERT INTO realtest(content) VALUES($1)', [content])];
+            case 3:
+                _a.sent();
+                console.log("'".concat(content, "' \uCD94\uAC00 \uC644\uB8CC"));
+                return [3 /*break*/, 6];
+            case 4:
+                err_1 = _a.sent();
+                console.error('쿼리 실행 오류:', err_1);
+                return [3 /*break*/, 6];
+            case 5:
+                client.release(); // 연결 종료
+                return [7 /*endfinally*/];
+            case 6: return [2 /*return*/];
+        }
+    });
+}); });
 app.listen(port, function () {
     console.log("Server is running at http://localhost:".concat(port));
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import path from 'path';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: "postgres",
+  host: 'localhost',
+  database: 'shindatabase',
+  password: '1234',
+  port: 5432,
+});
 
 dotenv.config({ path: `${__dirname}/../../.env` });
 
@@ -12,9 +21,19 @@ app.use(express.json())//!w 집에서 확인
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // /send 경로에 대한 POST 요청 처리
-app.post('/send', (req, res) => {
+app.post('/send', async (req, res) => {
   const { content } = req.body;
   res.json({ content });
+  const client = await pool.connect(); // 연결을 기다림
+  try {
+    await client.query('INSERT INTO realtest(content) VALUES($1)', [content]);
+    console.log(`'${content}' 추가 완료`);
+  } catch (err) {
+    console.error('쿼리 실행 오류:', err);
+  } finally {
+    client.release(); // 연결 종료
+  }
+
 });
 
 app.listen(port, () => {
