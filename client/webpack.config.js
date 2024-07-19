012746dfe6f365881434c7a1c423b1b3c1d4d2f5
신지윤 <ranji12@naver.com>
2024-07-19 15:46:59 +0900

    설정 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,5 +5,25 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.bundle.js"
-  }
-}
+  },
+  mode: "development", // 또는 'production'
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
