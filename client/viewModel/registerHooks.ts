aaaa7631e0253831e88c23ff223734070ed4ede2
naviadev <qo2oq1020@gmail.com>
2024-07-24 16:45:58 +0900

    Feat | registerHooks.ts : register Form의 상태를 관리하고 Model을 호출하는 Hook 모듈 생성

---
+++ client/viewModel/registerHooks.ts
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+import fetchRegisterData from "client/model/auth/fetchRegisterData";
+
+import { CreateUserInterface } from "../../shared/DTO/SharedDTO";
+import * as validate from "../model/auth/validateRegisterData";
+
+const useRegisterHooks = () => {
+  const [email, setEmail] = useState<string>("");
+  const [id, setId] = useState<number>(1);
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordCheck, setPasswordCheck] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [birth, setBirth] = useState<Date>(new Date());
+  const [position, setPosition] = useState<number>(1);
+  const [joinDate, setJoinDate] = useState<Date>(new Date());
+
+  const handleRegister = () => {
+    const data: CreateUserInterface = {
+      email,
+      id: 1,
+      password,
+      name,
+      phone_number: phoneNumber,
+      address,
+      birth,
+      position: 1,
+      join_date: joinDate,
+    };
+
+    if (
+      validate.validateEmail(email) &&
+      validate.validateName(name) &&
+      validate.ValidatePassword(password) &&
+      validate.validateAddress(address) &&
+      validate.validateDate(birth) &&
+      password == passwordCheck
+    ) {
+      fetchRegisterData(data);
+    } else {
+      return false;
+    }
+    console.log("성공");
+  };
+
+  return {
+    email,
+    setEmail,
+    id,
+    setId,
+    name,
+    setName,
+    password,
+    setPassword,
+    passwordCheck,
+    setPasswordCheck,
+    phoneNumber,
+    setPhoneNumber,
+    address,
+    setAddress,
+    birth,
+    setBirth,
+    position,
+    setPosition,
+    joinDate,
+    setJoinDate,
+    handleRegister,
+  };
+};
+
+export default useRegisterHooks;
