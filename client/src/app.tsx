60303e366f39f7d08bb6ee564b706d6091f3b2d3
신지윤 <ranji12@naver.com>
2024-07-24 15:30:01 +0900

    Feat | input동적 생성, input값 처리

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,29 +1,65 @@
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-const App = () =>{
-  const [content, newContent] = useState('');
-  const [innerContent, newInnerContent] = useState('');
-  const send=()=>{
-    const data = {
-      content:content
-    }
-    fetch("http://localhost:3001/send",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify(data)
-    })
-    .then(response=>response.json())
-    .then(data => newInnerContent(innerContent+data.content))
-  }
-  return(
-  <div>
-    <input type="text" value={content} onChange={(element)=>newContent(element.target.value)}placeholder="내용을 입력하세요"/>
-    <button  onClick={send}>전송하기</button>
-    <div id="inner">{innerContent}</div>
-  </div>
-  )
+interface Column {
+  column_name: string;
 }
+
+const App: React.FC = () => {
+  const [inputData, setInputData] = useState<Record<string, string>>({}); //사용자가 입력한 값
+  const [columns, setColumns] = useState<Column[]>([]); //서버에서 가져온 데이터
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/users')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setColumns(data);
+      })
+      .catch((err) => {
+        console.error('Error fetching users:', err);
+      });
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInputData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const send = () => {
+    fetch('http://localhost:3001/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputData)
+    })
+      .then(response => response.json())
+      .then(data => console.log(data.message))
+      .catch((err) => {
+        console.error('send fetch error', err)
+      })
+  }
+
+  return (
+    <div>
+      {columns.map((column) => (
+        <div key={column.column_name}>
+          <label>
+            {column.column_name}:
+            <input type="text" name={column.column_name} value={inputData[column.column_name]} onChange={handleChange} />
+          </label>
+        </div>
+      ))}
+      <button onClick={send}>전송하기</button>
+    </div>
+  );
+};
 
 export default App;
