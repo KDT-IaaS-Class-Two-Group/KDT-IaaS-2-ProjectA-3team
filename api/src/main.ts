7a7a9b620ea1483410cac3a1b7ab50c089ac5c2d
naviadev <qo2oq1020@gmail.com>
2024-07-24 16:46:58 +0900

    Fix | main.ts : cors 3000 port 허용

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,12 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.API_PORT);
 }
 bootstrap();
