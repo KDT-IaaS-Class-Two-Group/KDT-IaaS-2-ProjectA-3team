8f970813ccc7a90248aafa327c87dbb261fc048b
naviadev <qo2oq1020@gmail.com>
2024-07-24 12:41:35 +0900

    Feat | register.controller.ts : register 엔드포인트

--- api/src/auth/register.controller.ts
+++ api/src/auth/register.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { RegisterService } from './register.service';
-
+import { CreateUserInterface } from '@shared/DTO/SharedDTO';
 @Controller('register')
-export class AppController {
+export class RegsiterController {
   constructor(private readonly registerService: RegisterService) {}
 
   @Post()
-  getHello(): string {
-    return '';
+  async register(@Body() data: CreateUserInterface) {
+    try {
+      await this.registerService.registerUser(data);
+      return { message: '성공' };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
