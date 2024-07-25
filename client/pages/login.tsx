a75b49dd5ace63f6390776454adf73abf66f94f5
naviadev <qo2oq1020@gmail.com>
2024-07-25 16:57:04 +0900

    Feat | index.tsx, login.tsx, register.tsx : pages 기능을 통한 기본적인 라우팅 제공

---
+++ client/pages/login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "client/components/authPage/loginForm";
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <LoginForm></LoginForm>
+    </div>
+  );
+};
+
+export default LoginPage;
