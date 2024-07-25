f30ff5ee094e03b20b327a38c63724b9a2ee7ea6
naviadev <qo2oq1020@gmail.com>
2024-07-25 16:42:53 +0900

    Feat | auth.module.ts : auth 관련 모듈 명세 및 미들웨어 추가

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { RegisterModule } from './auth/register.module';
+import { LoggingMiddleware } from './logger.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [RegisterModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
