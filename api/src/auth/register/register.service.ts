3b2bcb05c55efb8be32caaa3b44f29c822dc77ad
naviadev <qo2oq1020@gmail.com>
2024-07-25 16:41:42 +0900

    Rename | register.*.ts : 경로수정 및 moduel 병합 - auth.module.ts

---
+++ api/src/auth/register/register.service.ts
@@ -0,0 +1,67 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { DatabaseService } from 'src/database.service';
+import { CreateUserInterface } from '@shared/DTO/SharedDTO';
+import { AuthService } from '../auth.service';
+@Injectable()
+export class RegisterService {
+  constructor(
+    private readonly databaseService: DatabaseService,
+    private readonly authServer: AuthService,
+  ) {}
+
+  async registerUser(data: CreateUserInterface) {
+    const duplicateResult = await this.authServer.findUserEmailInDatabase(
+      data.email,
+    );
+    //null이 아니라면, 즉 값이 포함되었다면 아래 if문을 들어가 에러를 리턴.
+    if (duplicateResult) {
+      throw new HttpException('중복된 사용자', HttpStatus.BAD_REQUEST);
+    }
+
+    const {
+      email,
+      id,
+      password,
+      name,
+      phone_number,
+      address,
+      birth,
+      position,
+      join_date,
+    } = data;
+    const params = [
+      email,
+      id,
+      password,
+      name,
+      phone_number,
+      address,
+      birth,
+      position,
+      join_date,
+    ];
+
+    const text = `
+    INSERT INTO users (
+        email, 
+        id, 
+        password, 
+        name, 
+        phone_number, 
+        address, 
+        birth, 
+        position, 
+        join_date
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
+    try {
+      await this.databaseService.query(text, params);
+    } catch {
+      throw new InternalServerErrorException('사용자 등록 실패');
+    }
+  }
+}
