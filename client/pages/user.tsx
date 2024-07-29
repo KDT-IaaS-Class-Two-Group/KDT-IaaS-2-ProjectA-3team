a5e7cafae255838f1311213f2bfe168411633bfe
신지윤 <ranji12@naver.com>
2024-07-29 17:56:01 +0900

    확인용 user 부모 리액트

---
+++ client/pages/user.tsx
@@ -0,0 +1,11 @@
+import UserHome from "../components/authPage/mainHeader";
+
+const UserPage: React.FC = () => {
+  return (
+    <div>
+      <UserHome></UserHome>
+    </div>
+  );
+};
+
+export default UserPage;
