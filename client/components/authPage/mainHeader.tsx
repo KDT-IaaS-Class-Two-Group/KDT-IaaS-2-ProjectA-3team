0520843065ef2c9616632426648cd431e4bbdd2f
신지윤 <ranji12@naver.com>
2024-07-29 17:55:16 +0900

    확인용 컴포넌트 작성

---
+++ client/components/authPage/mainHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as styles from "../../styles/user.css";
+
+const mainHeader: React.FC = () => {
+  return (
+    <div className={styles.header}>
+      <h1>Hello User</h1>
+    </div>
+  );
+};
+
+export default mainHeader;
