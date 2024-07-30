ef0e7950b2fa853e01e320893167c914e1ef0944
신지윤 <ranji12@naver.com>
2024-07-30 14:38:21 +0900

    user layout left css 완료

--- client/styles/user.css.ts
+++ client/styles/user.css.ts
@@ -1,5 +1,129 @@
 import { style } from "@vanilla-extract/css";
 
+export const mainPage = style({
+  display:'flex',
+  width:"20vw",
+  height:"100vh"
+})
+export const mainLeft =style({
+  width:"20vw",
+  height:"100vh"
+})
+
+export const mainHeader = style ({
+  width:"80vw",
+})
+
+export const leftAllContentFooter = style({
+  display:'flex',
+  flexDirection:'column',
+  gap:'30px',
+})
+export const leftHeader = style({
+  display:"flex",
+  gap:"20px",
+  border:"1px solid"
+})
+
+export const leftHeaderIcon = style({
+  alignContent:"center"
+})
+
+export const leftContent = style({
+  border : '1px solid',
+  padding:'10px',
+  display:'flex',
+  gap:'10px',
+  flexDirection:'column'
+})
+
+export const leftContentName = style({
+  display:'flex',
+  border:'1px solid',
+  gap:'5px'
+})
+
+export const leftContentList = style({
+  display:'flex'
+})
+
+export const leftContentIconList = style({
+  listStyleType:"none",
+  padding: '5px',
+  margin: 0
+})
+
+export const leftContentTextList = style({
+  listStyleType:"none",
+  padding: '5px',
+  margin: 0
+})
+
+export const leftContentNotiList = style({
+  listStyleType:"none",
+  padding: '5px',
+  margin: 0
+})
+
+export const listItem = style({
+  marginBottom:'10px'
+})
+
+export const leftFooter = style({
+  display:'flex',
+  flexDirection:'column',
+  border : '1px solid',
+  padding:'10px',
+  justifyContent:'center',
+  alignItems:'center',
+  height : '60vh'
+})
+
+export const leftFooterTitle = style({
+  textAlign:'center'
+})
+
+export const leftFooterSearch = style({
+  display:'flex',
+  backgroundColor:'gray',
+  width:'13vw',
+  height:'3vh',
+  gap:'3px',
+  alignItems:'center'
+})
+
+export const leftFooterManager = style({
+  display:'flex',
+  flexDirection:'column',
+  border:'1px solid',
+  width:'13vw',
+  alignItems:'center'
+})
+
+export const leftFooterManagerTitle = style({
+  display:'flex'
+})
+
+export const Managerstar = style({
+  alignContent:'center'
+})
+
+export const leftFooterUser = style({
+  display:'flex',
+  flexDirection:'column',
+  border:'1px solid',
+  width:'13vw',
+  alignItems:'center'
+})
+
+export const leftFooterUserTitle = style({
+  display:'flex'
+})
+
+export const UserStar = style({
+  alignContent:'center'
+})
+
 export const header = style({
   marginBottom: "20px",
 });
