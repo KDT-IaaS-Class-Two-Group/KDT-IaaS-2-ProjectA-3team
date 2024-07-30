1f3b0ab802fcffaf23630904e731ea957204e491
신지윤 <ranji12@naver.com>
2024-07-30 15:41:18 +0900

    컴포넌트 조립

--- client/pages/user.tsx
+++ client/pages/user.tsx
@@ -1,9 +1,19 @@
 import UserHome from "../components/authPage/mainHeader";
 
+import LeftContent from "../components/authPage/userLeftContent";
+
+import * as styles from "../styles/user.css";
+
+
 const UserPage: React.FC = () => {
   return (
-    <div>
-      <UserHome></UserHome>
+    <div className={styles.mainPage}>
+      <div className={styles.mainLeft}>
+        <LeftContent />
+      </div>
+      <div className={styles.mainContent}>
+        <UserHome />
+      </div>
     </div>
   );
 };
