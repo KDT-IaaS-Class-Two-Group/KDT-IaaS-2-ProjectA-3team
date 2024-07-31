feb472d3a533c0f63b37776d2a4dd43d7e6b37e2
신지윤 <ranji12@naver.com>
2024-07-31 16:13:21 +0900

    legacy폴더  삭제

--- legacy/legacy_client/src/app.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import LoginForm from "./component/auth/loginForm";
-const App: React.FC = () => {
-  return <LoginForm />;
-};
-
-export default App;
--- legacy/legacy_client/src/model/auth/validate/validateName.ts
+++
@@ -1,6 +0,0 @@
-const ValidateName = (name: string): boolean => {
-  const regex = /^[가-힣]{2,8}$/;
-  return regex.test(name);
-};
-
-export default ValidateName;
