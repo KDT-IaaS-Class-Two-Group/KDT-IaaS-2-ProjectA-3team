c480261e8425f2cbf2eda96888f1963e8fdf26a7
jiyun <ranji12@naver.com>
2024-07-31 23:19:01 +0900

    서버 service 작성

--- auth/src/api/auth/notice/notice.service.ts
+++ auth/src/api/auth/notice/notice.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
+import { NoticeDTO } from '../../../../../shared/DTO/DbDTO'
 
 
 @Injectable()
 export class NoticeService {
-
-}
+    private readonly uri = "mongodb://localhost:27017";
+    private readonly client = new MongoClient(this.uri);
+    async createNotice(NoticeDTO: NoticeDTO) {
+        try {
+          await this.client.connect();
+          const database = this.client.db('notice');
+          const collection = database.collection('noticeTable');
+          
+          const currentDate = new Date();
+          const noticeData = {
+            ...NoticeDTO,
+            createdAt: currentDate, // 현재 날짜와 시간 추가
+          };
+          
+          const result = await collection.insertOne(noticeData);
+          return `New document inserted with _id: ${result.insertedId}`;
+        } finally {
+          await this.client.close();
+        }
+      }
+    }
