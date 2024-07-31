23603e6c6825104cc58ddc970a07845e823bc01c
naviadev <qo2oq1020@gmail.com>
2024-07-31 15:43:27 +0900

    Fix | SharedDTO.ts: 수정된 ERD에 부합하도록 DTO 수정

--- shared/DTO/SharedDTO.ts
+++ shared/DTO/SharedDTO.ts
@@ -1,26 +1,36 @@
-export interface RegisterDataDTO {
-  email: string;
-  id: number;
-  password: string;
-  name: string;
-  phone_number: string;
+export interface SessionDTO {  
+  user_id : string;
+  username: string;
+  birth_date: string;
   address: string;
-  birth: Date;
-  position?: number;
-  join_date: Date;
-}
-export interface SessionDTO {
+  phone: string;
   email: string;
-  id: number;
-  name: string;
-  phone_number: string;
-  address: string;
-  birth: Date;
-  position?: number;
-  join_date: Date;
 }
 
 export interface LoginDTO {
+  email: string;
+  password: string;
+}
+
+export interface UserDTO {
+  user_id: number;
+  username: string;
+  birth_date: string;
+  address: string;
+  phone: string;
+  email: string;
+  password: string;
+  role_name: string;
+  salary: number;
+  field_name: string;
+}
+
+export interface PendingUserDTO {
+  user_id: string;
+  username: string;
+  birth_date: string;
+  address: string;
+  phone: string;
   email: string;
   password: string;
 }
