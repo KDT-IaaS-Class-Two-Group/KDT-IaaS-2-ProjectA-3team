16004350f93a76725916b2e95faa882e6f317c44
신지윤 <ranji12@naver.com>
2024-08-01 17:38:51 +0900

    테이블을 가져올때 NoticeDTO 객체 명시가 필요해서 추가

--- auth/src/api/auth/notice/notice.service.ts
+++ auth/src/api/auth/notice/notice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { MongoClient } from 'mongodb';
+import { MongoClient, WithId, Document } from 'mongodb';
 import { NoticeDTO } from '../../../../../shared/DTO/DbDTO';
 import { dateSet } from './utils/dateUtils';
 
@@ -7,21 +7,34 @@ import { dateSet } from './utils/dateUtils';
 export class NoticeService {
   private readonly uri = 'mongodb://localhost:27017';
   private readonly client = new MongoClient(this.uri);
-  async createNotice(NoticeDTO: NoticeDTO) {
+
+  async createNotice(noticeDTO: NoticeDTO){
     try {
       await this.client.connect();
       const database = this.client.db('notice');
-      const collection = database.collection('noticeTable');
+      const collection = database.collection<NoticeDTO>('noticeTable');
 
       const currentDate = new Date();
       const custom = dateSet(currentDate);
       const noticeData = {
-        ...NoticeDTO,
+        ...noticeDTO,
         createdAt: custom, // 현재 날짜와 시간 추가
       };
 
       const result = await collection.insertOne(noticeData);
       return `New document inserted with _id: ${result.insertedId}`;
+    } finally {
+      await this.client.close();
+    }
+  }
+
+  async getNotices(){
+    try {
+      await this.client.connect();
+      const collection = this.client.db('notice').collection<NoticeDTO>('noticeTable');
+
+      // NoticeDTO 타입으로 직접 반환
+      return await collection.find().toArray();
     } finally {
       await this.client.close();
     }
