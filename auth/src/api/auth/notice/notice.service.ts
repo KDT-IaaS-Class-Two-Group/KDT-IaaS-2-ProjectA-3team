d4c80b946162db02317cdf5e07752e6e30509172
신지윤 <ranji12@naver.com>
2024-08-01 10:34:52 +0900

    날짜와 시간 커스텀 후 저장

--- auth/src/api/auth/notice/notice.service.ts
+++ auth/src/api/auth/notice/notice.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { MongoClient } from 'mongodb';
-import { NoticeDTO } from '../../../../../shared/DTO/DbDTO'
-
+import { NoticeDTO } from '../../../../../shared/DTO/DbDTO';
+import { dateSet } from './utils/dateUtils';
 
 @Injectable()
 export class NoticeService {
-    private readonly uri = "mongodb://localhost:27017";
-    private readonly client = new MongoClient(this.uri);
-    async createNotice(NoticeDTO: NoticeDTO) {
-        try {
-          await this.client.connect();
-          const database = this.client.db('notice');
-          const collection = database.collection('noticeTable');
-          
-          const currentDate = new Date();
-          const noticeData = {
-            ...NoticeDTO,
-            createdAt: currentDate, // 현재 날짜와 시간 추가
-          };
-          
-          const result = await collection.insertOne(noticeData);
-          return `New document inserted with _id: ${result.insertedId}`;
-        } finally {
-          await this.client.close();
-        }
-      }
+  private readonly uri = 'mongodb://localhost:27017';
+  private readonly client = new MongoClient(this.uri);
+  async createNotice(NoticeDTO: NoticeDTO) {
+    try {
+      await this.client.connect();
+      const database = this.client.db('notice');
+      const collection = database.collection('noticeTable');
+
+      const currentDate = new Date();
+      const custom = dateSet(currentDate)
+      const noticeData = {
+        ...NoticeDTO,
+        createdAt: custom, // 현재 날짜와 시간 추가
+      };
+
+      const result = await collection.insertOne(noticeData);
+      return `New document inserted with _id: ${result.insertedId}`;
+    } finally {
+      await this.client.close();
     }
+  }
+}
