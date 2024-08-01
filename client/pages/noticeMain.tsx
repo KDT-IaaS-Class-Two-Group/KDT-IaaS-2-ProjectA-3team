80d442360844aa4d1760dbc3ce11d27e7b381729
신지윤 <ranji12@naver.com>
2024-08-01 14:00:21 +0900

    게시판 페이지 생성

---
+++ client/pages/noticeMain.tsx
@@ -0,0 +1,11 @@
+import NoticeMain from "client/components/authPage/noticeMainPage";
+
+const NoticeMainPage: React.FC = () => {
+  return (
+    <div>
+      <NoticeMain />
+    </div>
+  );
+};
+
+export default NoticeMainPage;
