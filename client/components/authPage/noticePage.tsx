7989ebd378d5e912e0ee955adbc44c326fc492f1
신지윤 <ranji12@naver.com>
2024-08-01 17:40:00 +0900

    서버에서 보내주는 것은 text인데 res.json으로 가져와서 오류남 res.text로 수정

--- client/components/authPage/noticePage.tsx
+++ client/components/authPage/noticePage.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import React, { useState } from "react";
 
 /**
  * * Function : RegisterForm
  * 작성자 : @신지윤 / 2024-07-31
- * 편집자 : @yun-21 / 2024-07-31
+ * 편집자 : @yun-21 / 2024-08-01
  * Issue :
  * @function : NoticeBoard
  * @description : 게시판
@@ -21,12 +22,17 @@ const NoticeBoard: React.FC = () => {
       },
       body: JSON.stringify({ title: state, content: stateContent }),
     })
-      .then((res) => res.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      })
       .then((data) => {
-        console.log("Success:", data);
+        console.log("Success", data);
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error("에러나어엉", error);
       });
   };
 
@@ -50,7 +56,9 @@ const NoticeBoard: React.FC = () => {
           rows={10}
         ></textarea>
       </div>
-      <button onClick={send}>작성하기</button>
+      <Link href="/noticeMain" passHref>
+        <button onClick={send}>작성하기</button>
+      </Link>
     </div>
   );
 };
