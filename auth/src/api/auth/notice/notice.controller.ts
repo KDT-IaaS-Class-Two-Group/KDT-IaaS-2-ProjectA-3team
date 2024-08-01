e64edd140d23416ce57795ec8ddca439ebae5991
신지윤 <ranji12@naver.com>
2024-08-01 10:34:31 +0900

    prettier의 오류로 수정

--- auth/src/api/auth/notice/notice.controller.ts
+++ auth/src/api/auth/notice/notice.controller.ts
@@ -1,8 +1,4 @@
-import {
-  Body,
-  Controller,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { NoticeService } from './notice.service';
 
@@ -10,10 +6,10 @@ import { NoticeDTO } from '@shared/DTO/DbDTO';
 
 @Controller('send')
 export class NoticeController {
-  constructor(private readonly noticeService: NoticeService){}
+  constructor(private readonly noticeService: NoticeService) {}
 
   @Post()
-  async noticeCreate(@Body() noticeDTO: NoticeDTO){
-    return await this.noticeService.createNotice(noticeDTO)
+  async noticeCreate(@Body() noticeDTO: NoticeDTO) {
+    return await this.noticeService.createNotice(noticeDTO);
   }
 }
