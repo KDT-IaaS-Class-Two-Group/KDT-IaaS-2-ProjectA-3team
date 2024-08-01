567a2bca9819a7ef10dd2361febd829d9e0727f2
dalramjwi <dalramjwi@gmail.com>
2024-08-01 15:58:24 +0900

    Feat|함수 형태로 슈도코드 작성

--- client/pages/team.tsx
+++ client/pages/team.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+function insertTeam() {
+  //e.value가 li jsx로 만들어져서 상단 상태 팀장에 추가
+}
+
+function deleteTeam() {
+  //e.value가 li jsx로 만들어져서 li를 ��제
+}
 const Team: React.FC = () => {
   return (
     <div>
@@ -7,24 +14,21 @@ const Team: React.FC = () => {
         <label htmlFor="teamName">팀 이름:</label>
         <input type="text" id="teamName" name="teamName" />
       </div>
-<div>팀장 : </div>
+      <div>팀장 : </div>
       <div>
-        <li onClick={이 리스트의 text를 팀장 div에 list 형식으로 삽입}>팀장1</li>
-        <li>팀장2</li>
+        <li onClick={insertTeam}>팀장1</li>
+        <li onClick={insertTeam}>팀장2</li>
       </div>
       <div>팀원 : </div>
-
       <div>
-        <lionClick={이 리스트의 text를 팀원 div에 list 형식으로 삽입}>팀원1</lionClick=>
-        <li>팀원2</li>
+        <li onClick={insertTeam}>팀원1</li>
+        <li onClick={insertTeam}>팀원2</li>
       </div>
 
       <div>
         <label htmlFor="teamDescription">팀 특징 서술:</label>
         <textarea id="teamDescription" name="teamDescription"></textarea>
       </div>
-      <div>추가</div>
-      <div>삭제</div>
       <button>전송</button>
     </div>
   );
