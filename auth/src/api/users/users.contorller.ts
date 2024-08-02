c3ea3192bfe4c485c0acb9f9093517e67374474f
naviadev <qo2oq1020@gmail.com>
2024-08-02 17:59:44 +0900

    Feat | users.controller.ts : pending_users 테이블의 데이터를 가져오기 위한 엔드포인트 구성

--- auth/src/api/users/users.contorller.ts
+++ auth/src/api/users/users.contorller.ts
@@ -2,29 +2,43 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { QueryBuilder } from 'src/database/queryBuilder';
 @Controller('/getUser')
+/**
+ * * Class : UsersController
+ * 작성자 : @naviadev / 2024-08-02
+ * 편집자 : @naviadev / 2024-08-02
+ * Issue :
+ * @class UsersController
+ * @param private readonly queryBuilder: QueryBuilder
+ * @description  : PIPE 를 통해 세션 검사 모듈 추가
+ */
 export class UsersController {
   constructor(private readonly queryBuilder: QueryBuilder) {}
 
   @Get('/all')
-  async CheckUser() {
+  async CheckUser(@Body() data) {
+    console.log(data);
     const obj = this.queryBuilder.SELECT(['*'], 'users').execution();
-    // -> SELECT * FROM users; 가 된다. 쿼리문 조합기
+    return obj;
+  }
+
+  @Get('pending')
+  async CheckPendingUser() {
+    const obj = this.queryBuilder.SELECT(['*'], 'pending_users').execution();
     return obj;
   }
 }
-
 
 @Controller('/team')
 /**
  * * Class : TeamController
  * 작성자 : @dalarmjwi / 2024-08-01
  * 편집자 : @naviadev / 2024-08-02
- * Issue : 
+ * Issue :
  * @class TeamController
  * @param private readonly queryBuilder: QueryBuilder
- * @description 
- * 
- * CHECKLIST 
+ * @description
+ *
+ * CHECKLIST
  * [ ] 1. 팀 데이터베이스 레코드 생성.
  * [ ] 2. 관계 테이블 레코드 생성. (for ?)
  * [ ] 3. 결과값 반환
