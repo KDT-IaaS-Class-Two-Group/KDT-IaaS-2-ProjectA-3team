a4a648ee56ae443ad8812279ac333d42dccf211f
naviadev <qo2oq1020@gmail.com>
2024-08-02 12:56:48 +0900

    Feat | middleware.ts : /admin , /user 분기 처리 및 인증 미들웨어 작성
    fetch를 통해 해당 세션 식별자의 role을 반환받고 이를 확인함으로써 특정 페이지에 접근하려는 요청들을 검열한다.

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
+import { ResponseJson } from "../shared/DTO/SharedDTO";
 /**
  * * Function : middleware
  * 작성자 : @naviadev / 2024-07-31
@@ -11,10 +13,10 @@ import type { NextRequest } from "next/server";
  */
 export const middleware = async (req: NextRequest) => {
   const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith("/project")) {
+  console.log(pathname);
+  if (pathname.startsWith("/admin") || pathname.startsWith("/user")) {
+    console.log(pathname);
     const cookieHeader = req.headers.get("cookie") || "";
-    console.log(cookieHeader);
     const res = await fetch("http://localhost:3001/verify-session", {
       method: "GET",
       headers: {
@@ -27,16 +29,32 @@ export const middleware = async (req: NextRequest) => {
       const url = new URL("/login", req.url);
       return NextResponse.redirect(url);
     }
+
+    const response: ResponseJson = await res.json();
+    const userRole = response.role;
+
+    if (pathname.startsWith("/admin")) {
+      if (userRole === "admin") {
+        return NextResponse.next();
+      } else {
+        const url = new URL("/login", req.url);
+        return NextResponse.redirect(url);
+      }
+    }
+
+    if (pathname.startsWith("/user")) {
+      if (userRole === "admin" || userRole === "user") {
+        return NextResponse.next();
+      } else {
+        const url = new URL("/login", req.url);
+        return NextResponse.redirect(url);
+      }
+    }
   }
 
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ["/project/:path*"],
+  matcher: ["/admin/:path*", "/user/:path*"],
 };
-
-// dispatchEvent
-// 비공개 상태 토큰 -> ?
-// Browser 저장용량
-// 갈아끼우는 형태. 검증된 상태로
