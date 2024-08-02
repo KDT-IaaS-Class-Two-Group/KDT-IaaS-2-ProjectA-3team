ddcd90fcf6f3e357e09e4431be1eabf2fa82545e
신지윤 <ranji12@naver.com>
2024-08-02 12:47:03 +0900

    Feat|사용자 로그인 main 레이아웃 추가

--- client/pages/project/main.tsx
+++ client/pages/project/main.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 
 import * as style from 'client/styles/project/root.css';
 import Main from 'client/components/main/mainComponent';
-import Side from 'client/components/side/sideContainer';
+import Side from 'client/components/authPage/userLeftContent';
 import Modal from 'client/components/modal/modal';
+import Content from 'client/components/authPage/mainHeader'
+
 const test: React.FC = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,6 +15,7 @@ const test: React.FC = () => {
   return (
     <div className={style.root}>
       <Side></Side>
+      <Content />
       <Main openModal={openModal}></Main>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <h2>모달</h2>
