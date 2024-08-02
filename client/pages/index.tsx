f27ac609defa801f6306f2c9d399db5d6c42436a
naviadev <qo2oq1020@gmail.com>
2024-08-02 12:58:05 +0900

    Fix | index.tsx : passhref 삭제

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,10 +6,10 @@ const MainPage: React.FC = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>DelaN</h1>
-      <Link href="/login" passHref>
+      <Link href="/login">
         <button className={styles.buttonLink}>로그인</button>
       </Link>
-      <Link href="/register" passHref>
+      <Link href="/register">
         <button className={styles.buttonLink}>회원가입</button>
       </Link>
       <div className={styles.content}>
