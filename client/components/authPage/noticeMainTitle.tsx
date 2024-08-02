6afdf6196c50f01245fbef7a5b7ae7344794158d
신지윤 <ranji12@naver.com>
2024-08-02 11:56:02 +0900

    Feat|issue18 게시판 파일들 복사해서 가져옴

---
+++ client/components/authPage/noticeMainTitle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoticeMainTitle = () => {
+  return (
+    <div>
+      <div>게시판</div>
+    </div>
+  );
+};
+
+export default NoticeMainTitle;
---
+++ client/pages/noticeMain.tsx
@@ -0,0 +1,21 @@
+import NoticeTitle from "client/components/authPage/noticeMainTitle";
+import NoticeContent from "client/components/authPage/noticeMainContent";
+import NoticeFooter from "client/components/authPage/noticeMainFooter";
+
+const NoticeMainPage: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <NoticeTitle />
+      </div>
+      <div>
+        <NoticeContent />
+      </div>
+      <div>
+        <NoticeFooter />
+      </div>
+    </div>
+  );
+};
+
+export default NoticeMainPage;
