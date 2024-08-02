04f80a212dfbc084e3bcc9b2db2fec36819deaeb
naviadev <qo2oq1020@gmail.com>
2024-08-02 09:51:36 +0900

    Feat | Modal 구현
    버튼을 통해 열고 닫을 수 있는 Modal 창 구현.
    현재 Overlay 영역을 누르면 Modal창이 닫힐 수 있도록 기능 수정 필요.

---
+++ client/components/main/mainComponent.tsx
@@ -0,0 +1,13 @@
+import * as style from "client/styles/project/main.css";
+interface mainProps {
+  openModal: () => void;
+}
+const Main: React.FC<mainProps> = ({ openModal }) => {
+  return (
+    <div className={style.container}>
+      <button onClick={openModal}>프로젝트 생성</button>
+    </div>
+  );
+};
+
+export default Main;
