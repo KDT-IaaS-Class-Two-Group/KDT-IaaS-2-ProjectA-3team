6e1752a273f4b88a630b214635e5d2a0ca91abe8
naviadev <qo2oq1020@gmail.com>
2024-08-02 19:59:22 +0900

    Style | *.ENUM.ts : ENUM을 통해 자주 사용되는 리터럴을 상수화

---
+++ client/ts/enum/REQUEST_METHOD.ENUM.ts
@@ -0,0 +1,4 @@
+enum REQUEST_METHOD {
+  __GET = "GET"
+}
+export default REQUEST_METHOD;
---
+++ client/ts/enum/REQUEST_URL.ENUM.ts
@@ -0,0 +1,4 @@
+enum REQUEST_URL {
+  __VERIFY_SESSION = "http://localhost:3001/verify-session"
+}
+export default REQUEST_URL;
