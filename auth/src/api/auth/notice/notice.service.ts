2ac1c7c277c8ad91859943ffb74d5a62cd45c3a6
신지윤 <ranji12@naver.com>
2024-08-03 23:41:48 +0900

    Feat|세션아이디 게시판과 연동 중

--- auth/src/api/auth/notice/notice.service.ts
+++ auth/src/api/auth/notice/notice.service.ts
@@ -8,7 +8,7 @@ export class NoticeService {
   private readonly uri = 'mongodb://localhost:27017';
   private readonly client = new MongoClient(this.uri);
 
-  async createNotice(noticeDTO: NoticeDTO) {
+  async createNotice(noticeDTO: NoticeDTO, user_id: string) {
     try {
       await this.client.connect();
       const database = this.client.db('notice');
@@ -19,6 +19,7 @@ export class NoticeService {
       const noticeData = {
         ...noticeDTO,
         createdAt: custom, // 현재 날짜와 시간 추가
+        user_id,
       };
 
       const result = await collection.insertOne(noticeData);
@@ -31,12 +32,12 @@ export class NoticeService {
   async getNotices() {
     try {
       await this.client.connect();
-      const collection = this.client
+      const mongoCollection = this.client
         .db('notice')
         .collection<NoticeDTO>('noticeTable');
 
       // NoticeDTO 타입으로 직접 반환
-      return await collection.find().toArray();
+      return await mongoCollection.find().toArray();
     } finally {
       await this.client.close();
     }
