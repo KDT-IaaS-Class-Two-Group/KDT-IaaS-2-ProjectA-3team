cd77d192b6941b47120d8f556fb95e6516ee00fd
신지윤 <ranji12@naver.com>
2024-08-05 11:50:07 +0900

    Refactor|게시판 조립 관리자 게시물 부분 추가

--- client/pages/noticeMain.tsx
+++ client/pages/noticeMain.tsx
@@ -1,12 +1,16 @@
 import NoticeTitle from 'client/components/userMainPage/noticeMainTitle';
 import NoticeContent from 'client/components/userMainPage/noticeMainContent';
 import NoticeFooter from 'client/components/userMainPage/noticeMainFooter';
+import NoticeAuthContent from 'client/components/authNotice/noticeAuthContent';
 
 const NoticeMainPage: React.FC = () => {
   return (
     <div>
       <div>
         <NoticeTitle />
+      </div>
+      <div>
+        <NoticeAuthContent />
       </div>
       <div>
         <NoticeContent />
