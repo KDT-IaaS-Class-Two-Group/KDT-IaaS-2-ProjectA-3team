e2974d43b300ee1a5691c97feabb0155448f8c51
naviadev <qo2oq1020@gmail.com>
2024-08-05 09:36:01 +0900

    Style | Request_url.enum.ts : fetch 주소 상수화

--- client/ts/enum/REQUEST_URL.ENUM.ts
+++ client/ts/enum/REQUEST_URL.ENUM.ts
@@ -1,4 +1,6 @@
 enum REQUEST_URL {
-  __VERIFY_SESSION = "http://localhost:3001/verify-session"
+  __VERIFY_SESSION = "http://localhost:3001/verify-session", 
+  __GET_PENDING_USERS = "http://localhost:3001/getUser/pending",
+  __LOGIN = "http://localhost:3001/login"
 }
 export default REQUEST_URL;
