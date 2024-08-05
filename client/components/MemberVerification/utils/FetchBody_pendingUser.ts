9239098825118013ad1d576078e91d0d51d297c7
naviadev <qo2oq1020@gmail.com>
2024-08-05 12:27:57 +0900

    Refactor | FetchBody_PendingUser.ts : Fetch 2번째 인자로 들어오는 객체를 상수화 시킴.

---
+++ client/components/MemberVerification/utils/FetchBody_pendingUser.ts
@@ -0,0 +1,10 @@
+export const AdminFetchBody = (item : { [key: string]: any }) => {
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include' as RequestCredentials,
+    body: JSON.stringify(item),
+  };
+};
