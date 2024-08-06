4a293e958706099a3df108dcaaf7e552cd36bdcf
신지윤 <ranji12@naver.com>
2024-08-06 17:46:56 +0900

    Refactor|게시판 원상복귀 후 제목에 링크추가

--- client/components/userMainPage/noticeMainContent.tsx
+++ client/components/userMainPage/noticeMainContent.tsx
@@ -1,43 +1,22 @@
-import {
-  modalBackdrop,
-  modalContent,
-  closeButton,
-} from 'client/styles/modal/modal.css';
 import React, { useState, useEffect } from 'react';
-
+import Link from 'next/link';
 interface ListNotice {
+  _id: string;
   title: string;
-  createdAt: string;
-  content: string; // 내용 필드 추가
+  content: string;
 }
 
-interface ModalProps {
-  isOpen: boolean;
-  notice: ListNotice | null;
-  onClose: () => void;
-}
+/**
+ * * Function : NoticeMainContent
+ * 작성자 : @yun-21 / 2024-08-01
+ * 편집자 : @yun-21 / 2024-08-01
+ * Issue : yun-21
+ * @function NoticeMainContent
+ * @description 
 
-const NoticeModal: React.FC<ModalProps> = ({ isOpen, notice, onClose }) => {
-  if (!isOpen || !notice) return null;
-
-  return (
-    <div className={modalBackdrop}>
-      <div className={modalContent}>
-        <button className={closeButton} onClick={onClose}>X</button>
-        <h2>{notice.title}</h2>
-        <p>{notice.content}</p>
-        <p>{notice.createdAt}</p>
-        <button onClick={SuJung}>수정</button>
-        <button onClick={Delete}>삭제</button>
-      </div>
-    </div>
-  );
-};
-
+ */
 const NoticeMainContent = () => {
-  const [userList, setUserList] = useState<ListNotice[]>([]);
-  const [selectedNotice, setSelectedNotice] = useState<ListNotice | null>(null); // 선택된 게시물 상태 관리
-  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 열림 상태 관리
+  const [userList, setUserList] = useState<ListNotice[]>([]); // empolyee 서버에서 건너오는 게시물 데이터
 
   useEffect(() => {
     const fetchNotices = () => {
@@ -52,45 +31,21 @@ const NoticeMainContent = () => {
           console.error('데이터를 가져오는 중 오류 발생:', err);
         });
     };
-    fetchNotices();
+    fetchNotices(); //컴포넌트가 처음 렌더링될 때 데이터 fetch
   }, []);
-
-  const SuJung = () => {
-    
-  }
-
-  const Delete = () => {
-
-  }
-
-  const handleTitleClick = (notice: ListNotice) => {
-    setSelectedNotice(notice); // 선택된 게시물 설정
-    setIsModalOpen(true); // 모달 열기
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false); // 모달 닫기
-    setSelectedNotice(null); // 선택된 게시물 초기화
-  };
-
   return (
     <div>
       {userList.length > 0 ? (
-        userList.map((notice, index) => (
-          <div key={index}>
-            <h3 onClick={() => handleTitleClick(notice)}>{notice.title}</h3>
-            <p>{notice.createdAt}</p>
+        userList.map((notice) => (
+          <div key={notice._id}>
+            <Link href={`/notice/${notice._id}`}>
+              <h3>{notice.title}</h3>
+            </Link>
           </div>
         ))
       ) : (
         <div>게시물 없음</div>
       )}
-
-      <NoticeModal 
-        isOpen={isModalOpen} 
-        notice={selectedNotice} 
-        onClose={handleCloseModal} 
-      />
     </div>
   );
 };
