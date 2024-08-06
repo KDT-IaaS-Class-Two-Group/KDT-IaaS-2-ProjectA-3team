d1c1b1f3f9bb743849b4eb17ae273e37c4afed7a
naviadev <qo2oq1020@gmail.com>
2024-08-06 14:18:39 +0900

    Feat | pending_process.*.ts : 대기중인 회원 승인 및 취소 엔드포인트 구성
    1. pending-process 요청을 처리하는 컨트롤러 구성.
    /approve, /cancle 요청을 처리한다.
    
    2. 승인 로직 :
    - 승인 버튼 클릭.
    - 해당 회원 정보를 엔드포인트로 fetch
    - pending_users 테이블에서 해당 사용자 id가 존재하는 지 재검토.
    - users 테이블로 이동 .
    - 기존 pending_users 에 존재하던 데이터 삭제.
    - defalut 권한 부여

--- auth/src/api/Pending_Process/pending_process.controller.ts
+++ auth/src/api/Pending_Process/pending_process.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { ApproveService } from './approve/approve.service';
 import { PendingUserDTO } from '../auth/register/DTO/PendingUserDTO';
 
@@ -7,9 +14,24 @@ export class PendingProcessController {
   constructor(private readonly approveService: ApproveService) {}
 
   @Post('/approve')
+  @HttpCode(200)
   async approveUser(@Body() data: PendingUserDTO) {
-    console.log(data);
+    try {
+      const approveResult = this.approveService.approve(data);
+      if (approveResult) {
+        return { message: '승인 성공' };
+      } else {
+        return { message: '승인 실패' };
+      }
+    } catch (error) {
+      console.error('ApproveUser 에러 : ', error);
+      throw new HttpException(
+        'Approve User 실패',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
+
   @Post('/cancle')
   cancleUser() {}
 }
