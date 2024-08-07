15ac8a4b68956b00ab8d2309a207682ba1e6a207
dalramjwi <dalramjwi@gmail.com>
2024-08-07 17:42:24 +0900

    Feat|해당 div에 맞게 스타일 삽입

--- client/styles/index.css.ts
+++ client/styles/index.css.ts
@@ -1,5 +1,6 @@
-// styles.css.ts
+// template.css.ts
 import { style } from "@vanilla-extract/css";
+import { Colors, FontSize, Size, Spacing } from "./standard.css";
 
 export const container = style({
   display: "flex",
@@ -7,35 +8,66 @@ export const container = style({
   alignItems: "center",
   justifyContent: "center",
   minHeight: "100vh",
+  backgroundColor: Colors.BackgroundSecondary,
+  color: Colors.FontSecondary,
   textAlign: "center",
-  padding: "20px",
+});
+
+export const login = style({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  width: Size.full,
+  marginBottom: Spacing.medium,
+  gap: Spacing.medium,
+});
+
+export const backcontainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: Colors.FontPrimary,
+  padding: "10vw 15vw",
+  borderRadius: "1.5vw",
+});
+
+export const buttoncontainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: Spacing.small,
 });
 
 export const heading = style({
-  fontSize: "2rem",
-  marginBottom: "20px",
+  fontSize: FontSize.XXLarge,
+  color: Colors.FontSecondary,
+  marginBottom: Spacing.large,
 });
 
 export const buttonLink = style({
-  background: "none",
+  fontWeight: "bold",
+  background: Colors.FontThrid,
   border: "none",
-  color: "#0070f3",
-  textDecoration: "underline",
+  color: Colors.FontSecondary,
   cursor: "pointer",
-  padding: "10px 20px",
-  fontSize: "1rem",
-  margin: "10px 0",
-
+  padding: Spacing.medium,
+  fontSize: FontSize.Medium,
+  margin: Spacing.medium,
+  borderRadius: Size.border,
+  width: "20vw",
   ":hover": {
-    textDecoration: "none",
+    background: Colors.StatusBlue,
   },
 });
 
 export const content = style({
-  marginTop: "20px",
-  padding: "20px",
-  border: "1px solid #ccc",
-  borderRadius: "5px",
-  width: "100%",
+  marginTop: Spacing.medium,
+  padding: Spacing.medium,
+  border: `1px solid ${Colors.Icon}`,
+  borderRadius: Size.border,
+  width: Size.full,
   maxWidth: "600px",
 });
