4ca5543405b161e687d1f06506b4e993ac57e968
dalramjwi <dalramjwi@gmail.com>
2024-08-07 17:42:06 +0900

    Feat|디자인 시안에 맞게 div 구성

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,17 +3,21 @@ import Link from "next/link";
 import * as styles from "../styles/index.css";
 const MainPage: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>
-        <div className={styles.container}>
-          <img src="/delan.png" />
-          <h1 className={styles.heading}>DelaN</h1>
-          <Link href="/login">
-            <button className={styles.buttonLink}>로그인</button>
-          </Link>
-          <Link href="/register">
-            <button className={styles.buttonLink}>회원가입</button>
-          </Link>
+        <div className={styles.backcontainer}>
+          <div className={styles.login}>
+            <img src="/delan.png" />
+            <h1 className={styles.heading}>DelaN</h1>
+          </div>
+          <div className={styles.buttoncontainer}>
+            <Link href="/login">
+              <button className={styles.buttonLink}>Sign in</button>
+            </Link>
+            <Link href="/register">
+              <button className={styles.buttonLink}>Sign up</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
