bc20f7b46ede29e1233e7735e5bf46018b2e1a56
dalramjwi <dalramjwi@gmail.com>
2024-08-07 17:41:50 +0900

    Feat|기본 css 객체 생성

--- client/styles/standard.css.ts
+++ client/styles/standard.css.ts
@@ -1,8 +1,8 @@
-// colors.ts
 export enum Colors {
   // 폰트 색상
   FontPrimary = "#171717", // 주 폰트 색상
   FontSecondary = "#ffffff", // 보조 폰트 색상
+  FontThrid = "#159FFF", //기본 하늘색
 
   // 배경 색상
   BackgroundDefault = "#f7f7f7", // 기본 배경 색상
@@ -27,9 +27,22 @@ export enum Colors {
 }
 
 export enum FontSize {
+  XXLarge = "3rem", // 48px
+  XLarge = "2rem", // 32px
   Huge = "1.5rem", // 24px
   Large = "1.25rem", // 20px
   Medium = "0.9375rem", // 15px
   Small = "0.75rem", // 12px
   Tiny = "0.3125rem", // 5px
 }
+export const Size = {
+  full: "100%",
+  low: "10%",
+  empty: "1%",
+  border: "0.5vw",
+};
+export const Spacing = {
+  small: "0.5rem", // 8px
+  medium: "1rem", // 16px
+  large: "1.5rem", // 24px
+};
