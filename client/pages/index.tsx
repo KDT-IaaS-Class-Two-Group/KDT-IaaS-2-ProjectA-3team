5536c8690386c84c7e5dfb5e1a8811468a6a9126
dalramjwi <dalramjwi@gmail.com>
2024-08-07 16:50:24 +0900

    Feat|img 추가 및 div 변경

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import * as styles from '../styles/index.css';
-
+import * as styles from "../styles/index.css";
 const MainPage: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>DelaN</h1>
-      <Link href="/login">
-        <button className={styles.buttonLink}>로그인</button>
-      </Link>
-      <Link href="/register">
-        <button className={styles.buttonLink}>회원가입</button>
-      </Link>
-      <div className={styles.content}>
-        <h2>TEST INDEX PAGE</h2>
-        <p>야호</p>
+    <div>
+      <div>
+        <div className={styles.container}>
+          <img src="/delan.png" />
+          <h1 className={styles.heading}>DelaN</h1>
+          <Link href="/login">
+            <button className={styles.buttonLink}>로그인</button>
+          </Link>
+          <Link href="/register">
+            <button className={styles.buttonLink}>회원가입</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
