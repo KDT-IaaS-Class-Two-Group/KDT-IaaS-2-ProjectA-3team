730d8fc1f12c220e0b488da1a2bca7fa567a76c3
dalramjwi <dalramjwi@gmail.com>
2024-08-07 18:45:29 +0900

    login css를 위한 div 요소 구성

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,9 +1,16 @@
 import LoginForm from "../components/auth_Component/login/loginForm";
+import * as styles from "../styles/index.css";
 
 const LoginPage: React.FC = () => {
   return (
-    <div>
-      <LoginForm></LoginForm>
+    <div className={styles.container}>
+      <div className={styles.backcontainer}>
+        <div className={styles.login}>
+          <img src="/delan.png" />
+          <h1 className={styles.heading}>DelaN</h1>
+        </div>
+        <LoginForm></LoginForm>
+      </div>
     </div>
   );
 };
