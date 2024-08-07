c6d7c408e0dea1fc361cd1e0d5388ecda077e734
dalramjwi <dalramjwi@gmail.com>
2024-08-07 15:34:10 +0900

    Feat|standard 형태 지정 : enum ; 본래 객체 형태로 진행했는데 고정된 값이기에 enum으로 변형했다.

---
+++ client/styles/standard.css.ts
@@ -0,0 +1,35 @@
+// colors.ts
+export enum Colors {
+  // 폰트 색상
+  FontPrimary = "#171717", // 주 폰트 색상
+  FontSecondary = "#ffffff", // 보조 폰트 색상
+
+  // 배경 색상
+  BackgroundDefault = "#f7f7f7", // 기본 배경 색상
+  BackgroundSecondary = "#1e1e1e", // 어두운 배경 색상
+  BackgroundAlt = "#dcdcdc", // 대체 배경 색상
+
+  // 아이콘 색상
+  Icon = "#676767", // 아이콘 색상
+
+  // 상태 색상
+  StatusYellow = "#ffd233", // 노란색
+  StatusLightYellow = "#ffe484", // 연한 노란색
+  StatusGreen = "#12b981", // 녹색
+  StatusLightGreen = "#3ce0a9", // 연한 녹색
+  StatusRed = "#f24e1e", // 빨간색
+  StatusLightRed = "#ff8a68", // 연한 빨간색
+  StatusPink = "#db2877", // 분홍색
+  StatusPurple = "#4621D6", // 보라색
+  StatusLightPurple = "#643cff", // 연한 보라색
+  StatusBlue = "#3049d1", // 파란색
+  StatusLightBlue = "#4a64ed", // 연한 파란색
+}
+
+export enum FontSize {
+  Huge = "1.5rem", // 24px
+  Large = "1.25rem", // 20px
+  Medium = "0.9375rem", // 15px
+  Small = "0.75rem", // 12px
+  Tiny = "0.3125rem", // 5px
+}
