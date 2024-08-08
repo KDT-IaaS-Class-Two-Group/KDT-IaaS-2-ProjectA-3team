578da7b9fa3818c1397a31bb318ae2f719fa4d1d
신지윤 <ranji12@naver.com>
2024-08-09 00:22:08 +0900

    Feat|관리자 게시판 라우터 안에서 Link 처리완료

---
+++ client/pages/noticeAuthAllPage.tsx
@@ -0,0 +1,9 @@
+import NoticeAuthAllContent from 'client/components/authNotice/noticeAuthAllContent';
+
+export default function render() {
+  return (
+    <div>
+      <NoticeAuthAllContent />
+    </div>
+  );
+}
