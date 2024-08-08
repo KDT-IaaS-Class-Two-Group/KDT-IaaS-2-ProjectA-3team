636b8cc33354b3fde698b83be4c9990c54d3a15d
신지윤 <ranji12@naver.com>
2024-08-08 17:49:01 +0900

    Feat|사용자 게시판 클라이언트 부분 페이징 작성

--- client/components/userMainPage/noticeMainContent.tsx
+++ client/components/userMainPage/noticeMainContent.tsx
@@ -18,29 +18,38 @@ interface ListNotice {
  */
 const NoticeMainContent = () => {
   const [userList, setUserList] = useState<ListNotice[]>([]); // empolyee 서버에서 건너오는 게시물 데이터
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [totalPages, setTotalPages] = useState(1); // 총 페이지 수
+  const itemsPerPage = 5; // 한 페이지당 항목 수
 
   useEffect(() => {
     const fetchNotices = () => {
-      fetch('http://localhost:3001/notices')
+      fetch(`http://localhost:3001/notices?page=${currentPage}&limit=${itemsPerPage}`)
         .then((response) => {
           return response.json();
         })
-        .then((data: ListNotice[]) => {
-          setUserList(data);
+        .then((data: { notices: ListNotice[]; totalPages: number }) => {
+          setUserList(data.notices);
+          setTotalPages(data.totalPages); // 총 페이지 수 설정
         })
         .catch((err) => {
           console.error('데이터를 가져오는 중 오류 발생:', err);
         });
     };
     fetchNotices(); //컴포넌트가 처음 렌더링될 때 데이터 fetch
-  }, []);
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page); // 페이지 변경
+  };
+
   return (
     <div>
       {userList.length > 0 ? (
         userList.map((notice,index) => (
           <div key={notice._id}>
             <Link href={`/notice/${notice._id}`}>
-              <h3>{index+1}</h3>
+              <h3>{index+1+ (currentPage - 1) * itemsPerPage}</h3>
               <h3>{notice.title}</h3>
               <h3>{notice.user_id}</h3>
               <h3>{notice.createdAt}</h3>
@@ -50,6 +59,18 @@ const NoticeMainContent = () => {
       ) : (
         <div>게시물 없음</div>
       )}
+      {/* 페이징 버튼 UI */}
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+          <button
+            key={page}
+            onClick={() => handlePageChange(page)}
+            disabled={currentPage === page}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
