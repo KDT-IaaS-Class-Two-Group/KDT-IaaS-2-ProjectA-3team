4e83a255b7f6617703fe87d5b9edeb741bc1433a
신지윤 <ranji12@naver.com>
2024-08-09 00:19:45 +0900

    Feat|관리자 게시판 페이징 처리완료

--- api/src/api/Auth/notice/notice.controller.ts
+++ api/src/api/Auth/notice/notice.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post, Get, Req, Put, Delete, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Req,
+  Put,
+  Delete,
+  Param,
+  Query,
+} from '@nestjs/common';
 
 import { Request } from 'express';
 
@@ -6,25 +16,38 @@ import { NoticeService } from './notice.service';
 
 import { NoticeDTO } from '@shared/DTO/DbDTO';
 
-
 @Controller()
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {}
 
   //사용자게시판 fetch
   @Get('notices')
-  async getAllNotices(@Query('page') page: string = '1', @Query('limit') limit: string = '5') {
-    const pageNumber = parseInt(page, 10);
-    const limitNumber = parseInt(limit, 10);
+  async getAllNotices(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '5',
+  ) {
+    const pageNumber = parseInt(page, 10); // 10진수로 변환
+    const limitNumber = parseInt(limit, 10); // 10진수로 변환
     return await this.noticeService.getNotices(pageNumber, limitNumber);
   }
 
-  //관리자게시판 fetch
+  //관리자게시판 fetch (게시판 main 5개)
   @Get('authnotices')
   async getAuthNotices() {
     return await this.noticeService.getAuthNotices();
   }
-  
+
+  //관리자게시판 fetch (관리자 총 게시판)
+  @Get('authallnotices')
+  async getAuthAllNotices(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ) {
+    const pageNumber = parseInt(page, 10); // 10진수로 변환
+    const limitNumber = parseInt(limit, 10); // 10진수로 변환
+    return await this.noticeService.getAuthAllNotices(pageNumber, limitNumber);
+  }
+
   //게시물 작성 fetch
   @Post('send')
   async noticeCreate(@Body() noticeDTO: NoticeDTO, @Req() req: Request) {
@@ -35,7 +58,11 @@ export class NoticeController {
   }
 
   @Put('notice/:id')
-  async updateNotice(@Param('id') id: string, @Body() noticeDTO: NoticeDTO, @Req() req: Request) {
+  async updateNotice(
+    @Param('id') id: string,
+    @Body() noticeDTO: NoticeDTO,
+    @Req() req: Request,
+  ) {
     const session = req.session.user;
     const user_id = session?.user_id;
     const role = session?.role_name;
