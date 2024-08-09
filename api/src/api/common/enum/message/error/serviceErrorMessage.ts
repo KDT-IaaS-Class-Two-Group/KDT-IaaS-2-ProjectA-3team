ecd0ef05119f227c68dd461246cb0793a98ab5cd
naviadev <qo2oq1020@gmail.com>
2024-08-09 11:46:26 +0900

    Style | comon/enum/*.ts : 메세지를 enum으로 상수화하는 작업을 진행.

--- api/src/api/common/enum/message/error/serviceErrorMessage.ts
+++ api/src/api/common/enum/message/error/serviceErrorMessage.ts
@@ -2,4 +2,9 @@ export enum SERVICE_ERROR {
   __MIGRATION_FAILURE = 'Migration 실패',
   __SET_ROLE_FAILURE = '권한 추가 실패',
   __DELETE_FAILURE = '유저 삭제 실패',
+  __CHECK_TEAM_ERROR = '팀 체크 에러 - team name',
+  __DUPLICATION_TEAM_NAME = '해당 팀은 존재합니다.',
+  __SAVE_TEAM_FAILURE = '팀 정보 저장 실패',
+  __FETCHING_TEAM_FAILURE = '팀 정보를 가져오는 데 실패',
+  __FETCHING_TEAM_MEMBER = '팀원 데이터를 가져오는 데 실패',
 }
