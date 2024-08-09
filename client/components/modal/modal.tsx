e3fc473383ff4be07636b2078dec5ccf2e35ba03
leejeayoung <leejeayoung875@gmail.com>
2024-08-10 00:48:48 +0900

    Feat | 모달 컴포넌트 수정: 바깥 클릭 시 모달 닫힘 기능 추가

--- client/components/modal/modal.tsx
+++ client/components/modal/modal.tsx
@@ -8,8 +8,15 @@ import { ModalProps } from "./interface/props/modalProps.interface";
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
+
+  const handleBackdropClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <div className={modalBackdrop}>
+    <div className={modalBackdrop} onClick={handleBackdropClick}>
       <div className={modalContent}>
         <button className={closeButton} onClick={onClose}>
           x
