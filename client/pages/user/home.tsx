615b1fc265d81e73517b85b73a3be2e65c9e3340
test <leejeayoung875@gmail.com>
2024-08-09 17:49:10 +0900

    Feat|user sidebar로 변경

--- client/pages/user/home.tsx
+++ client/pages/user/home.tsx
@@ -4,16 +4,15 @@ import Side from "client/components/userMainPage/userLeftContent";
 import Content from "client/components/userMainPage/mainHeader";
 import Link from "next/link";
 import CalendarComponent from "client/components/Calendar/calendar";
-
+import UserSidebar from "../../components/SideBar/UserSidebar";
+import { mainpagecontainer } from "client/styles/admin/dashboardStyles.css";
 const UserHome: React.FC = () => {
   return (
-    <div className={style.root}>
-      <Side />
-      <Content>
-        <p>야호</p>
-        <CalendarComponent></CalendarComponent>
-      </Content>
-      <div>  
+    <div className={mainpagecontainer}>
+      <UserSidebar></UserSidebar>
+
+      <CalendarComponent></CalendarComponent>
+      <div>
         <button>
           <Link href={"/user/project/info"}>project</Link>
         </button>
