a08e89d326ca8665920b7499915dee91e032bbc3
test <leejeayoung875@gmail.com>
2024-08-09 15:26:44 +0900

    Feat|px 기반 기본 base text 템플릿 제작

---
+++ client/styles/standardtextsize.css.ts
@@ -0,0 +1,30 @@
+import { style } from "@vanilla-extract/css";
+import { Colors, FontSize, Spacing, Size } from "./standard.css";
+
+export const base48Text = style({
+  fontSize: FontSize.XXLarge,
+});
+
+export const base32Text = style({
+  fontSize: FontSize.XLarge,
+});
+
+export const base24Text = style({
+  fontSize: FontSize.Huge,
+});
+
+export const base20Text = style({
+  fontSize: FontSize.Large,
+});
+
+export const base15Text = style({
+  fontSize: FontSize.Medium,
+});
+
+export const base12Text = style({
+  fontSize: FontSize.Small,
+});
+
+export const base5Text = style({
+  fontSize: FontSize.Tiny,
+});
