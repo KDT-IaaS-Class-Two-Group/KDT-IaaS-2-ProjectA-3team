4f2f74dfb0aeb11d148f4862adc761c7b8f0bbba
naviadev <qo2oq1020@gmail.com>
2024-08-10 17:23:54 +0900

    Feat | memberList.tsx: 매개변수로 들어온 프로젝트 투입 인원을 출력할 수 있는 div

---
+++ client/components/project_info/style/memberList.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+
+export const MemberList = style({
+  width : '10vw',
+  height : '10vh',
+  textAlign : 'center',
+  alignItems: "center",
+  justifyContent : 'center',
+  display : 'flex',
+  border : 'solid 1px',
+  marginTop : '20px',
+  marginBottom : '20px',
+  borderRadius : '10px'
+})
