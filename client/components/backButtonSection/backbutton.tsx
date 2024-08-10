d0b56e42084f1f30da0b5e4c6548a392f2689bf4
naviadev <qo2oq1020@gmail.com>
2024-08-10 17:19:12 +0900

    Feat | backbutton.tsx : 값을 받고 컨텐츠, URL을 구성하여 이동할 수 있는 버튼 컴포넌트 추가

---
+++ client/components/backButtonSection/backbutton.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { BackButtonSection } from "./style/backbutton.css";
+import BackButtonProps from "./utils/props/backbutton.props";
+
+
+const BackButton : React.FC<BackButtonProps> = ({url , button_name}) => {
+  return (
+    <section className={BackButtonSection}>
+      <Link href={url}>
+        <button>
+          <span />
+          {button_name}
+        </button>
+      </Link>
+    </section>
+  );
+};
+export default BackButton;
