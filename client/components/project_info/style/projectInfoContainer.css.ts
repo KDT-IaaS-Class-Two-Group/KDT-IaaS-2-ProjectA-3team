584e22b672ee12bb577dfa2f26c6440331db76a2
naviadev <qo2oq1020@gmail.com>
2024-08-10 17:25:46 +0900

    Feat | projectContent.tsx : 프로젝트 상세 정보를 출력하기 위한 컴포넌트
    추가작업이 필요한 컴포넌트
    
    1. 스택 테이블 추가
    2. 스택 출력
    3. 이슈 테이블 출력

---
+++ client/components/project_info/style/projectInfoContainer.css.ts
@@ -0,0 +1,8 @@
+import { style } from "@vanilla-extract/css"
+export const ProjectInfoContainer = style({
+  width : '100%',
+  height : '100%',
+  display :'grid',
+  border : 'solid 1px',
+  borderRadius : '20px'
+})
