c197850f86b8efd225a5b7c14a446a8eb47ffa68
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-08-11 14:44:06 +0900

    Feat|logo 선택 시 메인 페이지로 이동

--- client/components/common/logo/Logo.tsx
+++ client/components/common/logo/Logo.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import * as styles from "../../../styles/sidebar/SidebarStyles.css";
-
-const Logo: React.FC = () => {
+interface LogoProps {
+  onClick: () => void;
+}
+const Logo: React.FC<LogoProps> = ({ onClick }) => {
   return (
-    <div className={styles.logocontainer}>
+    <div className={styles.logocontainer} onClick={onClick}>
       <div className={styles.logocontent}>
         <img src="/delan.png" alt="DelaN Logo" className={styles.logoimage} />
         <span className={styles.logotext}>DelaN</span>
