9343712afa9f18e5947cfadda865b055cad7b67d
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-08-11 14:43:31 +0900

    Feat|사이드 바 선택에  컴포 변화

--- client/pages/admin/home.tsx
+++ client/pages/admin/home.tsx
@@ -1,6 +1,8 @@
+import React, { useState } from "react";
 import {
   mainpagecontainer,
   contentcontainer,
+  maincontentcontainer,
 } from "client/styles/admin/admindashboard.css";
 import { fullRowSection } from "client/styles/admin/greet/greet.css";
 import AdminSidebar from "../../components/SideBar/AdminSidebar";
@@ -8,12 +10,22 @@ import MainHeader from "client/components/common/header/mainheader";
 import AdminMainContent from "client/components/adminMainPage/AdminMainPage";
 fullRowSection;
 const Dash: React.FC = () => {
+  // 사이드바에서 선택된 메뉴에 따라 표시할 컴포넌트 상태
+  const [currentComponent, setCurrentComponent] = useState<React.ReactNode>(
+    <AdminMainContent />
+  );
+
+  // 사이드바에서 메뉴를 클릭할 때 호출되는 함수
+  const handleMenuClick = (component: React.ReactNode) => {
+    setCurrentComponent(component);
+  };
+
   return (
     <div className={mainpagecontainer}>
-      <AdminSidebar />
+      <AdminSidebar onMenuItemClick={handleMenuClick} />
       <div className={contentcontainer}>
         <MainHeader />
-        <AdminMainContent />
+        <div className={maincontentcontainer}>{currentComponent}</div>
       </div>
     </div>
   );
