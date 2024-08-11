0e2dba7ae1405e2bc63ad79f10ea782422388114
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-08-11 14:18:38 +0900

    Feat|유저 사이드 바 함수화

--- client/components/SideBar/UserSidebar.tsx
+++ client/components/SideBar/UserSidebar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import * as styles from "../../styles/sidebar/SidebarStyles.css";
-import { Colors } from "../../styles/standard.css";
 import { UserSearch } from "../common/nav/UserSearch";
 import Logo from "../common/logo/Logo";
 
-const UserSidebar: React.FC = () => {
+interface UserSidebarProps {
+  onSelect: (component: string) => void;
+}
+
+const UserSidebar: React.FC<UserSidebarProps> = ({ onSelect }) => {
   return (
     <div className={styles.mainpagecontainer}>
       <div className={styles.sidebarcontainer}>
@@ -17,26 +20,35 @@ const UserSidebar: React.FC = () => {
           </div>
           <nav>
             <ul className={styles.menulist}>
-              <li className={styles.menuitem}>
+              <li
+                className={styles.menuitem}
+                onClick={() => onSelect("projects")}
+              >
                 <span className={styles.menuitemicon}>🏠</span>
                 <span>프로젝트 조회</span>
               </li>
-              <li className={styles.menuitem}>
+              <li
+                className={styles.menuitem}
+                onClick={() => onSelect("kanban")}
+              >
                 <span className={styles.menuitemicon}>👥</span>
                 <span>칸반보드</span>
               </li>
-              <li className={styles.menuitem}>
+              <li className={styles.menuitem} onClick={() => onSelect("board")}>
                 <span className={styles.menuitemicon}>📁</span>
                 <span>게시판</span>
               </li>
-              <li className={styles.menuitem}>
+              <li
+                className={styles.menuitem}
+                onClick={() => onSelect("profile")}
+              >
                 <span className={styles.menuitemicon}>📁</span>
                 <span>개인정보 조회</span>
               </li>
             </ul>
           </nav>
         </div>
-        <UserSearch></UserSearch>
+        <UserSearch />
       </div>
     </div>
   );
