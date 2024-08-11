788e2de50c695c247d32ab166fdc127fca158574
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-08-11 14:06:51 +0900

    Feat| main page 컴포넌트화 성공

---
+++ client/components/adminMainPage/AdminMainPage.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import {
+  mainpagecontainer,
+  contentcontainer,
+  section,
+  projectSection,
+  buttonSection,
+  title,
+  cardHeader,
+  cardContent,
+  proceedingButton,
+  allTasksButton,
+  maincontentcontainer,
+} from "client/styles/admin/admindashboard.css";
+import Project from "client/components/auth_Component/project/project";
+import PendingUser from "client/components/MemberVerification/utils/PendingUser";
+import StateUsers from "client/components/StateUsers";
+import Attendance from "client/components/attendance";
+import NoticeBoard from "client/components/Notice/NoticeBoard";
+import DatabaseGUI from "client/components/DatabaseGuI";
+import { attendanceSection } from "client/styles/admin/workAttendance/workattendance.css";
+import { noticeBoardSection } from "client/styles/admin/noticeBoard/noticeboard.css";
+import { databaseGUISection } from "client/styles/admin/databaseGUI/databasegui.css";
+import { requestSection } from "client/styles/admin/requests/requests.css";
+import PendingUsersList from "client/components/PendingUsersList";
+import CheckUsersCount from "client/components/checktest";
+
+const AdminMainContent: React.FC = () => {
+  return (
+    <div className={maincontentcontainer}>
+      <div className={`${section} ${projectSection}`}>
+        <div className={cardContent}>
+          <Project />
+        </div>
+      </div>
+
+      <div className={`${section} ${requestSection}`}>
+        <div className={cardHeader}>Requests</div>
+        <CheckUsersCount />
+      </div>
+
+      <div className={`${section} ${requestSection}`}>
+        <div className={cardHeader}>User Management</div>
+        <PendingUsersList />
+      </div>
+
+      <div className={`${section} ${attendanceSection}`}>
+        <div className={cardHeader}>Work Attendance</div>
+        <div className={cardContent}>
+          <Attendance />
+        </div>
+      </div>
+
+      <div className={`${section} ${noticeBoardSection}`}>
+        <div className={cardHeader}>Notice Board</div>
+        <div className={cardContent}>
+          <NoticeBoard />
+        </div>
+      </div>
+
+      <div className={`${section} ${databaseGUISection}`}>
+        <div className={cardHeader}>Database GUI</div>
+        <div className={cardContent}>
+          <DatabaseGUI />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AdminMainContent;
--- client/pages/admin/home.tsx
+++ client/pages/admin/home.tsx
@@ -1,38 +1,11 @@
-import Link from "next/link";
 import {
   mainpagecontainer,
   contentcontainer,
-  section,
-  projectSection,
-  buttonSection,
-  title,
-  cardHeader,
-  cardContent,
-  proceedingButton,
-  allTasksButton,
-  maincontentcontainer,
 } from "client/styles/admin/admindashboard.css";
-import Project from "client/components/auth_Component/project/project";
-import PendingUser from "client/components/MemberVerification/utils/PendingUser";
-import StateUsers from "client/components/StateUsers";
-import Attendance from "client/components/attendance";
-import NoticeBoard from "client/components/Notice/NoticeBoard";
-import DatabaseGUI from "client/components/DatabaseGuI";
-import {
-  admintext,
-  basetext,
-  fullRowSection,
-  titlecontainer,
-  titletext,
-} from "client/styles/admin/greet/greet.css";
-import { attendanceSection } from "client/styles/admin/workAttendance/workattendance.css";
-import { noticeBoardSection } from "client/styles/admin/noticeBoard/noticeboard.css";
-import { databaseGUISection } from "client/styles/admin/databaseGUI/databasegui.css";
-import { requestSection } from "client/styles/admin/requests/requests.css";
+import { fullRowSection } from "client/styles/admin/greet/greet.css";
 import AdminSidebar from "../../components/SideBar/AdminSidebar";
 import MainHeader from "client/components/common/header/mainheader";
-import PendingUsersList from "client/components/PendingUsersList";
-import CheckUsersCount from "client/components/checktest";
+import AdminMainContent from "client/components/adminMainPage/AdminMainPage";
 fullRowSection;
 const Dash: React.FC = () => {
   return (
@@ -40,44 +13,7 @@ const Dash: React.FC = () => {
       <AdminSidebar />
       <div className={contentcontainer}>
         <MainHeader />
-        <div className={maincontentcontainer}>
-          <div className={`${section} ${projectSection}`}>
-            <div className={cardContent}>
-              <Project />
-            </div>
-          </div>
-
-          <div className={`${section} ${requestSection}`}>
-            <div className={cardHeader}>Requests</div>
-            <CheckUsersCount />
-          </div>
-
-          <div className={`${section} ${requestSection}`}>
-            <div className={cardHeader}>User Management</div>
-            <PendingUsersList />
-          </div>
-
-          <div className={`${section} ${attendanceSection}`}>
-            <div className={cardHeader}>Work Attendance</div>
-            <div className={cardContent}>
-              <Attendance />
-            </div>
-          </div>
-
-          <div className={`${section} ${noticeBoardSection}`}>
-            <div className={cardHeader}>Notice Board</div>
-            <div className={cardContent}>
-              <NoticeBoard />
-            </div>
-          </div>
-
-          <div className={`${section} ${databaseGUISection}`}>
-            <div className={cardHeader}>Database GUI</div>
-            <div className={cardContent}>
-              <DatabaseGUI />
-            </div>
-          </div>
-        </div>
+        <AdminMainContent />
       </div>
     </div>
   );
