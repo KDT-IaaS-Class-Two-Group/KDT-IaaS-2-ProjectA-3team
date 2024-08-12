18ae0667a2af1a88ae4f8834c577aa414451bf40
zoeznm <bomikim046@gmail.com>
2024-08-12 17:02:42 +0900

    st | database GUI grid 적용하기

--- client/styles/admin/databaseGUI/databasegui.css.ts
+++ client/styles/admin/databaseGUI/databasegui.css.ts
@@ -20,12 +20,23 @@ export const maincontainter = style({
 export const ulliststlye = style({
   listStyle: "none",
   textDecoration: "none",
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 300px)",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
 });
 
-
 export const astyle = style({
+  textAlign: "center",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "block",
   textDecoration: "none",
   backgroundColor: Colors.StatusYellow,
-  fontSize: FontSize.Medium,
-  color : Colors.FontPrimary,
+  height: "40px",
+  margin: "10px",
+  fontSize: "20px",
+  color: Colors.FontPrimary,
+  borderRadius: Size.border,
 });
