3ea8a9166725a20d45a9ef3d89b2cebdeb48a72b
naviadev <qo2oq1020@gmail.com>
2024-08-12 19:11:58 +0900

    Feat | addStackButton, settingStackModal.ts  : 모달, 버튼 구현 스택 추가를 위해 분리한 컴포넌트

--- client/components/project_info/item/addStackButton.tsx
+++ client/components/project_info/item/addStackButton.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-import Modal from "client/components/MODAL/modal";
 import StackSearch from "./settingStack";
+import Modal from "client/components/modal/modal";
+import { useRouter } from "next/router";
 
 const AddStackButton = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
+  const router = useRouter();
+  const { id, query } = router.query;
+
+  let projectName: string = "";
+  if (id !== undefined && !Array.isArray(id)) {
+    projectName = id;
+  }
 
   const onOpen = () => {
     setOpen(true);
@@ -16,7 +24,7 @@ const AddStackButton = () => {
     <div>
       <button onClick={onOpen}>Stack 추가</button>
       <Modal isOpen={isOpen} onClose={onClose}>
-        <StackSearch></StackSearch>
+        <StackSearch project_name = {projectName}></StackSearch>
       </Modal>
     </div>
   );
--- client/components/project_info/item/settingStack.tsx
+++ client/components/project_info/item/settingStack.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import { StackResult } from "../interface/stackResult.interface";
 import searchStacks from "../service/fetchSearchData";
+import fetchPostStack from "../service/fetchPostStack";
 
-const StackSearch = () => {
+interface Stack_Props {
+  project_name: string;
+}
+const StackSearch: React.FC<Stack_Props> = ({project_name}) => {
+  // query -> input 창에 존재하는 value 값 : fetch를 통해 전송되는 값
   const [query, setQuery] = useState("");
+  // result -> fetch를 통해 출력되는 검색 결과 값
   const [results, setResults] = useState<StackResult[]>([]);
+  // projectStack -> fetch 를 통해 Post 할 예정인 값.
+  const [projectStack, setProjectStack] = useState<StackResult[]>([]);
 
   const handleKeyDown = async (
     event: React.KeyboardEvent<HTMLInputElement>
@@ -15,8 +23,29 @@ const StackSearch = () => {
     }
   };
 
+  // [ ] 저장버튼 구현하기
   return (
     <div>
+      <div>
+        {projectStack.length > 0 ? (
+          <div>
+            <h2>저장된 스택</h2>
+            {projectStack.map((stack) => (
+              <p key={stack.stack_name}>{stack.stack_name}</p>
+            ))}
+          </div>
+        ) : (
+          <p>No saved stacks</p>
+        )}
+        <button
+          onClick={() => {
+            fetchPostStack(projectStack, project_name);
+          }}
+        >
+          {" "}
+          저장{" "}
+        </button>
+      </div>
       <input
         type="text"
         value={query}
@@ -26,20 +55,30 @@ const StackSearch = () => {
         onKeyDown={handleKeyDown}
         placeholder="Stack을 입력해주세요"
       />
-
       <div>
         {results.length > 0 ? (
           <ul>
             {results.map((stack, index) => (
-              <li key={index}>
-                {stack.stack_name} ({stack.stack_type})
-              </li>
+              <div>
+                <li key={index}>
+                  {stack.stack_name} ({stack.stack_type})
+                </li>
+                <button
+                  onClick={() => {
+                    const data = [...projectStack, stack];
+                    setProjectStack(data);
+                    console.log(projectStack);
+                  }}
+                >
+                  추가
+                </button>
+              </div>
             ))}
           </ul>
         ) : (
           <p>No results found</p>
         )}
-      </div> 
+      </div>
     </div>
   );
 };
