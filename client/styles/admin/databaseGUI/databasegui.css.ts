75c0a969bb1309088935290c380abedf25d63bea
zoeznm <bomikim046@gmail.com>
2024-08-12 16:48:41 +0900

    st | database gui style change

--- client/styles/admin/databaseGUI/databasegui.css.ts
+++ client/styles/admin/databaseGUI/databasegui.css.ts
@@ -6,3 +6,26 @@ export const databaseGUISection = style({
   gridColumn: "span 12",
   gridRow: "span 3",
 });
+
+export const maincontainter = style({
+  backgroundColor: Colors.FontPrimary,
+  width: Size.full,
+  height: Size.full,
+  borderRadius: Size.border,
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+});
+
+export const ulliststlye = style({
+  listStyle: "none",
+  textDecoration: "none",
+});
+
+
+export const astyle = style({
+  textDecoration: "none",
+  backgroundColor: Colors.StatusYellow,
+  fontSize: FontSize.Medium,
+  color : Colors.FontPrimary,
+});
