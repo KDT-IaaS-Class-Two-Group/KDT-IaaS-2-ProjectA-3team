986818bb95da1ab6f56c3a4a996edb2694323d26
naviadev <qo2oq1020@gmail.com>
2024-08-12 19:09:02 +0900

    Feat | Storage server
    정적 이미지를 서빙하는 Storage Server 작성

---
+++ storage/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "path";
+const cors = require('cors');
+
+const app = express();
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log(`Image server is running at http://localhost:3003`);
+});
