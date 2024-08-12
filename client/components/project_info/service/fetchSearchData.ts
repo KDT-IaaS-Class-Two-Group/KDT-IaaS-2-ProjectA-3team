4554fd09def133982f327d99620df8b2376db2c8
naviadev <qo2oq1020@gmail.com>
2024-08-12 19:10:56 +0900

    Feat | fetchPostStack.ts : stack 저장 모듈 추가

--- client/components/project_info/service/fetchSearchData.ts
+++ client/components/project_info/service/fetchSearchData.ts
@@ -1,13 +1,15 @@
 import { Dispatch, SetStateAction } from "react";
-import { StackResult } from "../interface/stackResult.interface"; 
+import { StackResult } from "../interface/stackResult.interface";
+import { FETCH_ERROR } from "client/ts/enum/error/FETCH_ERROR.enum";
+import REQUEST_URL from "client/ts/enum/request/REQUEST_URL.ENUM";
 
 const searchStacks = async (searchQuery: string, setResults: Dispatch<SetStateAction<StackResult[]>>) => {
   try {
-    const response = await fetch(`http://localhost:3001/stack/search/${searchQuery}`);
+    const response = await fetch(`${REQUEST_URL.__GET_SEARCH_STACK}/${searchQuery}`);
     const data: StackResult[] = await response.json();
     setResults(data);
   } catch (error) {
-    console.error('Stack을 찾는데 실패했습니다.', error);
+    console.error(FETCH_ERROR.__FAILURE_GET_STACK, error);
   }
 };
 
