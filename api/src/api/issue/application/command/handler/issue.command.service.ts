daaf7e1a009e538418d219af1de574e11e45304d
naviadev <qo2oq1020@gmail.com>
2024-08-13 14:59:08 +0900

    Feat | issue.command.service.ts : 이슈 도메인에 해당하는 명령을 실행하는 서비스 로직 작성

---
+++ api/src/api/issue/application/command/handler/issue.command.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { IssueRepository } from 'src/api/issue/infrastructure/repository/issue.repository';
+import { CreateIssueCommand } from '../createIssue.command';
+import { Issue } from 'src/api/issue/domain/entites/issue.entity';
+import { DeleteIssueCommand } from '../deleteIssue.command';
+import { SetStatusCommand } from '../setStatus.command';
+import { SetUserCommand } from '../setUser.commnad';
+
+@Injectable()
+export class IssueCommandHandler {
+  constructor(private readonly issueRepository: IssueRepository) {}
+
+  async handleCreateIssue(command: CreateIssueCommand): Promise<void> {
+    const { issue_name, project_name } = command;
+    console.log(issue_name);
+    console.log(project_name);
+    const issue: Issue = Issue.create(
+      undefined,
+      issue_name,
+      '대기',
+      project_name,
+      null, // 할당된 사람 없음
+    );
+
+    this.issueRepository.createIssue(issue);
+  }
+
+  async handleDeleteIssue(command: DeleteIssueCommand): Promise<void> {
+    const { issue_id } = command;
+    this.issueRepository.deleteIssue(issue_id);
+  }
+
+  async handleUpdateStatus(command: SetStatusCommand): Promise<void> {
+    const { issue_id, status } = command;
+    this.issueRepository.setStatus(issue_id, status);
+  }
+
+  async hadleSetUser(command: SetUserCommand): Promise<void> {
+    const { issue_id, user_id } = command;
+    this.issueRepository.setUser(issue_id, user_id);
+  }
+}
