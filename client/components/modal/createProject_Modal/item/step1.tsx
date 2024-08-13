cbf77d2788b58eb93328bc2fb88a1d172770d25c
dalramjwi <dalramjwi@gmail.com>
2024-08-13 15:56:17 +0900

    Feat|모달 창 css 수정

--- client/components/modal/createProject_Modal/item/step1.tsx
+++ client/components/modal/createProject_Modal/item/step1.tsx
@@ -1,4 +1,12 @@
+import {
+  base24Text,
+  base32Text,
+  bold24Text,
+  bold32Text,
+} from "client/styles/standardtextsize.css";
 import { SetStateAction, Dispatch } from "react";
+import { projectmodalbtn } from "../../style/modal.css";
+import { flexrowcontainer } from "client/styles/standardcontainer.css";
 
 interface StepProps {
   projectName: string;
@@ -13,17 +21,23 @@ const Step1: React.FC<StepProps> = ({
 }) => {
   return (
     <div>
-      <h2>프로젝트 생성</h2>
-      <p>Project Name</p>
-      <input
-        type="text"
-        value={projectName}
-        onChange={(e) => setProjectName(e.target.value)}
-        placeholder="프로젝트 이름"
-      />
-      <button onClick={handleNext} disabled={!projectName}>
-        다음
-      </button>
+      <div className={bold32Text}>프로젝트 생성</div>
+      <p className={bold24Text}>Project Name</p>
+      <div className={flexrowcontainer}>
+        <input
+          type="text"
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+          placeholder="프로젝트 이름"
+        />
+        <button
+          className={projectmodalbtn}
+          onClick={handleNext}
+          disabled={!projectName}
+        >
+          다음
+        </button>
+      </div>
     </div>
   );
 };
