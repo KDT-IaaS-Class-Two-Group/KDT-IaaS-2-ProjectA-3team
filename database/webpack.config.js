bcdc6e70e6e8f87e7d1f6f9d633856cacd1bbee6
신지윤 <ranji12@naver.com>
2024-08-14 12:31:00 +0900

    Feat|database웹팩에 의존성패키지 제외 후 모든 번들 실행 구문작성

--- database/webpack.config.js
+++ database/webpack.config.js
@@ -1,25 +1,47 @@
 const path = require('path');
 const glob = require('glob');
-module.exports={
-  entry: glob.sync('./src/notice/**/*.ts').reduce((all, filePath) => {
-    const entryName = filePath.replace('./src/', '').replace('.ts', '');
-    all[entryName] = filePath;
+
+module.exports = {
+  entry: glob.sync('./src/notice/**/**/*.ts').reduce((all, filePath) => {
+    const entryName = path.relative('src', filePath).replace('.ts', ''); 
+    all[entryName] = path.resolve(__dirname, filePath);
     return all;
   }, {}),
-  output:{
-    path:path.resolve(__dirname,"dist"),
-    filename:`[name].bundle.js`
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: `[name].bundle.js`,
+    clean: true,
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.ts$/,
-        use:"ts-loader",
-        exclude:/node_modules/
-      }
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
     ],
   },
-  resolve:{
-    extensions: [".ts",".js"]
-  }
-}
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  target: 'node', // Node.js 환경을 위한 설정
+  node: {
+    __dirname: false, // Node.js의 __dirname을 사용하도록 설정
+    __filename: false 
+  },
+  externals: {
+    kerberos: 'commonjs kerberos',
+    timers: 'commonjs timers',
+    '@mongodb-js/zstd': 'commonjs @mongodb-js/zstd',
+    '@aws-sdk/credential-providers': 'commonjs @aws-sdk/credential-providers',
+    'gcp-metadata': 'commonjs gcp-metadata',
+    snappy: 'commonjs snappy',
+    aws4: 'commonjs aws4',
+    'mongodb-client-encryption': 'commonjs mongodb-client-encryption',
+  },
+};
