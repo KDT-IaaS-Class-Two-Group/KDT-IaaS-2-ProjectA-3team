9c4af0abcf23d5ed8b72ec69a79489011fef6afd
dalramjwi <dalramjwi@gmail.com>
2024-08-14 09:15:34 +0900

    Feat|게시판 연결 사이드바 수정

--- client/components/SideBar/UserSidebar.tsx
+++ client/components/SideBar/UserSidebar.tsx
@@ -86,7 +86,10 @@ const UserSidebar: React.FC<UserSidebarProps> = ({ onMenuItemClick }) => {
                 text="칸반보드"
                 onClick={() => handleMenuItemClick(<KanbanBoard />)}
               />
-              <MenuItem text="게시판" link="/noticeMain" />
+              <MenuItem
+                text="게시판"
+                onClick={() => handleMenuItemClick(<NoticeMainPage />)}
+              />
               <MenuItem
                 text="개인정보 조회"
                 onClick={() => handleMenuItemClick(<UserPersonal />)}
