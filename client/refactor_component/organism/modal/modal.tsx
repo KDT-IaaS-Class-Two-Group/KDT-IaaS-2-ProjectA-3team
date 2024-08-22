70a1fda55b7157d4ad13ad8edc0a98d25ba474b6
naviadev <qo2oq1020@gmail.com>
2024-08-22 18:07:31 +0900

    Fix | modal : props 분리, extends cardProps 해제

--- client/refactor_component/organism/modal/modal.tsx
+++ client/refactor_component/organism/modal/modal.tsx
@@ -1,15 +1,7 @@
-import { modalBackdrop } from "client/components/modal/style/modal.css";
 import ModalBackdrop from "client/refactor_component/atom/back_drop/back_drop";
-import Button from "client/refactor_component/atom/button/button";
-import Card from "client/refactor_component/atom/card/card";
-import CardProps from "client/refactor_component/atom/card/props/card.props";
 import ModalButtonContainer from "client/refactor_component/molecule/modal_close_button/modal_close_button";
 import ModalContent from "client/refactor_component/molecule/modal_content/modal_content";
-
-interface ModalProps extends CardProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
+import ModalProps from "./props/modal.props";
 
 /**
  * @param ModalProps, CardProps
@@ -17,14 +9,8 @@ interface ModalProps extends CardProps {
  */
 // [ ] Modal 최상위 컨테이너 스타일 관련 위치 조정 필요 .
 // [ ] Modal Button 스타일 추가.
-const Modal: React.FC<ModalProps> = ({
-  isOpen,
-  onClose,
-  children,
-  container_style,
-}) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
-
   const handleBackdropClick = (event: React.MouseEvent) => {
     if (event.target === event.currentTarget) {
       onClose();
