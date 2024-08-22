678e3ac6e21505dbfa8dab8bc73c8edfbfc74f0b
naviadev <qo2oq1020@gmail.com>
2024-08-22 17:49:34 +0900

    Fix | back_drop : css 디렉토리 추가.

--- client/refactor_component/atom/back_drop/back_drop.tsx
+++ client/refactor_component/atom/back_drop/back_drop.tsx
@@ -1,6 +1,5 @@
-import { modalBackdrop } from "client/styles/modal/modal.css";
 import ModalBackdropProps from "./props/back_drop.props";
-// [ ] 스타일 위치 변경 필요. 현재 임의 css 추가
+import { modalBackdrop } from "./style/back_drop.css";
 const ModalBackdrop: React.FC<ModalBackdropProps> = ({ onClick, children }) => {
   return (
     <div className={modalBackdrop} onClick={onClick}>
---
+++ client/refactor_component/atom/back_drop/style/back_drop.css.ts
@@ -0,0 +1,13 @@
+import { style } from "@vanilla-extract/css";
+export const modalBackdrop = style({
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  zIndex: 999,
+});
