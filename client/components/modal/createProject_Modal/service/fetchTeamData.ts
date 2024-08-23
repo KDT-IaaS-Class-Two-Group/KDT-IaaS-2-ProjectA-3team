ed2bfe568077b78d82c49836b008472a526dcbf0
naviadev <qo2oq1020@gmail.com>
2024-08-23 14:47:24 +0900

    Refactor | Step3 -> project_team_set.tsx  : 모듈 분리, fetch 모듈 이동

--- client/components/modal/createProject_Modal/service/fetchTeamData.ts
+++ client/components/modal/createProject_Modal/service/fetchTeamData.ts
@@ -1,15 +1,15 @@
 import { FETCH_ERROR } from "client/ts/enum/error/FETCH_ERROR.enum";
 import REQUEST_URL from "client/ts/enum/request/REQUEST_URL.ENUM"
 
-const fetchTeamList = async()=>{
+const fetchTeamList = async () => {
   try {
-    const res= await fetch(REQUEST_URL.__GET_TEAM_ALL);
-    if(!res.ok){
+    const res = await fetch(REQUEST_URL.__GET_TEAM_ALL);
+    if (!res.ok) {
       throw new Error(FETCH_ERROR.__FAILURE_GET_TEAM);
     }
     const result = await res.json();
     return result;
-  }catch(error){
+  } catch (error) {
     throw error;
   }
 }
