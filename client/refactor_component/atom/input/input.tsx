8b21513b551cae47781a01bc02a4b4a1796b974b
naviadev <qo2oq1020@gmail.com>
2024-08-23 14:43:15 +0900

    Style | input.tsx: 누락된 Lint Style 적용

--- client/refactor_component/atom/input/input.tsx
+++ client/refactor_component/atom/input/input.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import InputProps from "./props/input.props";
 // [ ] input 기본 스타일 정의.
-const Input: React.FC<InputProps> = ({ id, type, value, onChange, placeholder }) => {
+const Input: React.FC<InputProps> = ({
+  id,
+  type,
+  value,
+  onChange,
+  placeholder,
+}) => {
   return (
-    <input className="" id={id} type={type} value={value} onChange={onChange} placeholder={placeholder} />
+    <input
+      className=""
+      id={id}
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
   );
 };
 export default Input;
