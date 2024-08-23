51b4b8e665ee681220edb8e39145dfc39ab22142
zoeznm <bomikim046@gmail.com>
2024-08-23 10:02:41 +0900

    생성

---
+++ client/atoms/card.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+interface CardProps {
+  container_style: string;
+  children?: ReactNode;
+}
+const Card: React.FC<CardProps> = ({ container_style, children }) => {
+  return <div className={container_style}>{children}</div>;
+};
+export default Card;
