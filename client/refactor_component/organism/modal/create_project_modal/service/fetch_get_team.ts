f9bd58721dd0a1b46d6b642e8ef70504e2230ec9
naviadev <qo2oq1020@gmail.com>
2024-08-23 14:52:30 +0900

    Refactor | fetch_get_team.ts,  fetch_post_project.ts : 요청 모듈 분리 및 Modal 레이어로 이동

---
+++ client/refactor_component/organism/modal/create_project_modal/service/fetch_get_team.ts
@@ -0,0 +1,16 @@
+import { FETCH_ERROR } from "client/ts/enum/error/FETCH_ERROR.enum";
+import REQUEST_URL from "client/ts/enum/request/REQUEST_URL.ENUM"
+
+const fetchTeamList = async () => {
+  try {
+    const res = await fetch(REQUEST_URL.__GET_TEAM_ALL);
+    if (!res.ok) {
+      throw new Error(FETCH_ERROR.__FAILURE_GET_TEAM);
+    }
+    const result = await res.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+export default fetchTeamList;
