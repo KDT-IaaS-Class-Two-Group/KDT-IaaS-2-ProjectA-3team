69ce277854cffc0eb35af88f442672e563271de8
dalramjwi <dalramjwi@gmail.com>
2024-08-23 13:02:51 +0900

    Refactor|interface 추가

--- client/components/team/interface/team.interface.ts
+++ client/components/team/interface/team.interface.ts
@@ -1,3 +1,11 @@
 export interface User {
   user_id: string;
 }
+export interface TeamNameFieldProps {
+  teamName: string;
+  setTeamName: (name: string) => void;
+}
+export interface TeamDescriptionFieldProps {
+  teamDescription: string;
+  setTeamDescription: (description: string) => void;
+}
