605bb092a72d2a4e43656fa6e29d120a58ddd423
신지윤 <ranji12@naver.com>
2024-08-23 18:03:37 +0900

    Refactor|댓글이 생성되는데 텀이 생기는 오류 발생 비동기처리로 해결

--- client/components/userMainPage/commentFormModule/fetchCommentSend.ts
+++ client/components/userMainPage/commentFormModule/fetchCommentSend.ts
@@ -1,14 +1,25 @@
-const commentSend = (postId: string, comment: string, setComment: React.Dispatch<React.SetStateAction<string>>, fetchComment: () => void) => {
-  // 댓글 추가 API 호출
-  fetch(`http://localhost:3001/comments/${postId}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-    body: JSON.stringify({ content: comment }),
-  });
-  setComment(""); // 폼 초기화
-  fetchComment(); // 댓글 추가 후 데이터 새로고침
+const commentSend = async (
+  postId: string,
+  comment: string,
+  setComment: React.Dispatch<React.SetStateAction<string>>,
+  fetchComment: () => void
+) => {
+  try {
+    // 댓글 추가 API 호출
+    await fetch(`http://localhost:3001/comments/${postId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ content: comment }),
+    });
+
+    setComment(""); // 폼 초기화
+    fetchComment(); // 댓글 추가 후 데이터 새로고침
+  } catch (error) {
+    console.error("댓글 작성 중 오류 발생:", error);
+  }
 };
+
 export default commentSend;
