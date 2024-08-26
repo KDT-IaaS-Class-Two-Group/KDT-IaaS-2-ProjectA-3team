2aa9645ed771e844a9b54b58a79181d94489d1c5
dalramjwi <dalramjwi@gmail.com>
2024-08-26 18:19:28 +0900

    Refactor|card header card btn 사용 버전으로 변경

--- client/refactor_component/molecule/card_header/card_header.tsx
+++ client/refactor_component/molecule/card_header/card_header.tsx
@@ -5,39 +5,31 @@
  * 주어진 타이틀을 텍스트로 표시하는 기능을 제공한다.
  *
  * @author @dalramjwi
- * @date 2024-08-23
+ * @date 2024-08-26
  */
 
 import React from "react";
 import CardHeaderProps from "./props/card_header.props";
 import Text from "client/refactor_component/atom/text/text";
-import Button from "client/refactor_component/atom/button/button";
-import { cardHeader } from "client/styles/admin/admindashboard.css";
+import { cardHeader, section } from "client/styles/admin/admindashboard.css";
+import CardButton from "../card_button/card_button";
+import { projectitletext } from "client/styles/admin/project/project.css";
 
 /**
- * @brief 카드 헤더 컴포넌트
- * @details title을 받아 카드의 헤더 부분에 텍스트를 렌더링하며 버튼이 포함될 수 있다.
+ * @brief 메인 페이지 카드 헤더 컴포넌트
+ * @details title을 받아 카드의 헤더 부분에 텍스트를 렌더링하며 버튼이 포함된다.
  * Text 컴포넌트를 사용하여 타이틀을 표시한다.
- * @param {CardHeaderProps} props 카드 헤더 컴포넌트가 받는 속성들 (title)
- * @return div 요소 안에 스타일이 적용된 타이틀 텍스트를 포함하여 반환
+ * @param {CardHeaderProps} props 카드 헤더 컴포넌트가 받는 속성들 (title, onButtonClick)
+ * @return div 요소 안에 스타일이 적용된 타이틀 텍스트와 버튼을 포함하여 반환
  */
-const CardHeader: React.FC<CardHeaderProps> = ({
-  title,
-  buttonText,
-  buttonStyle,
-  onButtonClick,
-}) => {
+const CardHeader: React.FC<CardHeaderProps> = ({ title, onButtonClick }) => {
   return (
-    <div className={cardHeader}>
-      <div className={""}>
-        <Text content={title} className={""} />
-        {buttonText && (
-          <Button
-            button_text={buttonText}
-            button_style={buttonStyle || ""}
-            onClick={(event) => onButtonClick?.(event)}
-          />
-        )}
+    <div className={section}>
+      <div className={cardHeader}>
+        <div className={""}>
+          <Text content={title} className={projectitletext} />
+          <CardButton onClick={onButtonClick} />
+        </div>
       </div>
     </div>
   );
