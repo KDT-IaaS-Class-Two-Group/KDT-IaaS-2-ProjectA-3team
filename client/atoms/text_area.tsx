2bfa0eb1a58d2bf358a2fe753c9cc06d9f5eeefc
dalramjwi <dalramjwi@gmail.com>
2024-08-27 10:07:48 +0900

    DEL| 중복된 파일 제거

--- client/atoms/text_area.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-interface TextAreaProps {
-  children: ReactNode;
-  className?: string;
-}
-const TextArea: React.FC<TextAreaProps> = ({ children, className }) => {
-  return <textarea className={className}>{children}</textarea>;
-};
-export default TextArea;
--- client/atoms/ul.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-interface UlProps {
-  ul_style: string;
-  children: ReactNode;
-}
-const Ul: React.FC<UlProps> = ({ ul_style, children }) => {
-  return <ul className={ul_style}>{children}</ul>;
-};
-export default Ul;
