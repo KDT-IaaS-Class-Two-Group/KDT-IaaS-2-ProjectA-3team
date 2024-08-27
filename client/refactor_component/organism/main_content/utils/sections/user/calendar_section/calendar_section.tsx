0f5ac97abaab490328a9671240c05be57474ef59
dalramjwi <dalramjwi@gmail.com>
2024-08-27 12:27:01 +0900

    Refactor | calendar 이동

---
+++ client/refactor_component/organism/main_content/utils/sections/user/calendar_section/calendar_section.tsx
@@ -0,0 +1,11 @@
+import withCardSection from "client/refactor_component/organism/card/utils/with_card_section";
+import React from "react";
+
+const CalendarSection = withCardSection(CalendarComponent, {
+  sectionClassName: `${section} ${usernoticesection}`,
+  title: "Calendar",
+  buttonText: undefined, // 캘린더에는 별도의 버튼이 필요 없을 수 있음
+  onClick: undefined, // 특별한 클릭 동작이 필요하지 않음
+});
+
+export default CalendarSection;
