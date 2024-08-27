33aa650734d9de460b71cbf4366493d337a80c68
naviadev <qo2oq1020@gmail.com>
2024-08-27 10:44:18 +0900

    Refactor | table.tsx : 테이블 컴포넌트 추가 - Organism Layer

---
+++ client/refactor_component/organism/table/table.tsx
@@ -0,0 +1,26 @@
+import {
+  ColumnDef,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import Tbody from "client/refactor_component/molecule/table/body/tbody";
+import Tfoot from "client/refactor_component/molecule/table/footer/tFooter";
+import Thead from "client/refactor_component/molecule/table/header/thead";
+interface TableProps<TData> {
+  data: [];
+  columns: ColumnDef<TData>[];
+}
+const Table = <TData,>({ columns, data }: TableProps<TData>) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <table>
+      <Thead headerGroups={table.getHeaderGroups()}></Thead>
+      <Tbody rowModel={table.getRowModel().rows}></Tbody>
+      <Tfoot footerGroups={table.getFooterGroups()}></Tfoot>
+    </table>
+  );
+};
