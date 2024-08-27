e3f0ba7fe408df6c6c1da2483c9ceebf28c55098
dalramjwi <dalramjwi@gmail.com>
2024-08-27 12:26:12 +0900

    Refactor | props 변경

--- client/refactor_component/organism/main_content/props/main_content.props.ts
+++ client/refactor_component/organism/main_content/props/main_content.props.ts
@@ -1,3 +1,11 @@
+import React from "react";
+export interface FollowingUserProps {
+  user_id: string;
+  username: string;
+  email: string;
+}
 export default interface MainContentProps {
   onClick: (component: React.ReactNode) => void;
+  userId: string;
+  followingUsers: FollowingUserProps[];
 }
