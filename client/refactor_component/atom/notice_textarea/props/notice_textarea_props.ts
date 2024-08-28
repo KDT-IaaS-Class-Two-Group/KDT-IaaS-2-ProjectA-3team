f9f925f3e531913815b7e07c18c5908e8d38317f
dalramjwi <dalramjwi@gmail.com>
2024-08-29 01:15:12 +0900

    Refactor| 중복되는 textarea 삭제 및 통합

--- client/refactor_component/atom/notice_textarea/props/notice_textarea_props.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEventHandler } from "react";
-export default interface TextAreaProps {
-    value: string;
-    onChange: ChangeEventHandler<HTMLTextAreaElement>;
-    name : string;
-    id : string;
-    placeholder : string;
-    style : string;
-}
--- client/refactor_component/atom/text_area/props/text_area.props.ts
+++ client/refactor_component/atom/text_area/props/text_area.props.ts
@@ -1,5 +1,9 @@
-import { ReactNode } from "react";
+import { ChangeEventHandler } from "react";
 export default interface TextAreaProps {
-  children: ReactNode;
-  className?: string;
+  value?: string; // 텍스트 영역의 현재 값
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>; // 값이 변경될 때 호출되는 함수
+  name?: string; // 텍스트 영역의 이름
+  id?: string; // 텍스트 영역의 ID
+  placeholder?: string; // 텍스트 영역의 placeholder 텍스트
+  className: string; // 텍스트 영역의 스타일 클래스 이름 (선택적)
 }
