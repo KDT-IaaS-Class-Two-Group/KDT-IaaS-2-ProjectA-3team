b6a2fc9956b477a602b3550ff961f85db3daec59
dalramjwi <dalramjwi@gmail.com>
2024-08-29 01:10:16 +0900

    Refactor| 불필요한 파일 삭제

--- client/refactor_component/atom/notice_input/props/notice_input_props.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEventHandler } from "react";
-export default interface InputProps {
-  id : string;
-  type: string;
-  value: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
-  placeholder : string;
-  style : string;
-}
