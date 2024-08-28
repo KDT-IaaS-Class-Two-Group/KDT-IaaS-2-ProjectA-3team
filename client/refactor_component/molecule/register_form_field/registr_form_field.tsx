4095193b71282c4e9ea88db245b06ebbaf89a995
dalramjwi <dalramjwi@gmail.com>
2024-08-29 01:20:16 +0900

    Refactor| 불필요한 중복된 input 삭제 및 경로 변경

--- client/refactor_component/molecule/register_form_field/registr_form_field.tsx
+++ client/refactor_component/molecule/register_form_field/registr_form_field.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Input from "client/refactor_component/atom/register_input/register_input";
 import Label from "client/refactor_component/atom/label/label";
 import FormFieldProps from "./props/registr_form_field.props";
+import Input from "client/refactor_component/atom/input/input";
 
 /**
  * @file form_field.tsx
  * @brief 폼 필드 컴포넌트 파일
  * @details 이 파일은 레이블과 입력 필드를 함께 렌더링하는 폼 필드 컴포넌트를 정의한다.
  * 사용자가 입력할 수 있는 폼 요소를 만들기 위해 레이블과 입력 필드를 결합하여 사용한다.
- * 
+ *
  * @author @zoeznm
  * @date 2024-08-25
  */
