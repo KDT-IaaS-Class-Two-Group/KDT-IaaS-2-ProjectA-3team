22e789cd108b2371adc3e79eeda4aab6f101e21a
dalramjwi <dalramjwi@gmail.com>
2024-08-29 01:09:52 +0900

    Refactor| 해당되는 noticepage의 input 수정 및 props 수정

--- client/components/userMainPage/noticePage.tsx
+++ client/components/userMainPage/noticePage.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import React, { useState } from "react";
 import * as styles from "../../styles/notice/notice.css";
 import send from "./noticePageModule/fetchSendNotice";
-import Input from "client/refactor_component/atom/notice_input/notice_input";
 import Button from "client/refactor_component/atom/button/button";
 import TextArea from "client/refactor_component/atom/notice_textarea/notice_textarea";
+import Input from "client/refactor_component/atom/input/input";
 
 /**
  * * Function : RegisterForm
@@ -45,7 +45,7 @@ const NoticeBoard: React.FC = () => {
             value={state}
             onChange={(ele) => setState(ele.target.value)}
             placeholder="글 제목"
-            style={styles.inputSize}
+            className={styles.inputSize}
           />
         </div>
         <div className={styles.testsize}>
--- client/refactor_component/atom/input/props/input.props.tsx
+++ client/refactor_component/atom/input/props/input.props.tsx
@@ -4,7 +4,7 @@ export default interface InputProps {
   type: string;
   value?: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
-  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
   placeholder: string;
   className?: string;
 }
