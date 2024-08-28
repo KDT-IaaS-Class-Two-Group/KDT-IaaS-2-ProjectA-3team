b38fdb316cd1e62fbd6496f677c4eebb8392cb6f
dalramjwi <dalramjwi@gmail.com>
2024-08-29 00:51:51 +0900

    Refactor| 추후 수정 필요한 파일 표시

--- client/refactor_component/atom/login_input/login_input.tsx
+++ client/refactor_component/atom/login_input/login_input.tsx
@@ -1,3 +1,5 @@
+//!!!!login_input 파일은 atom 폴더에 존재할 것이 아님. 이동 필요
+//!! [ ] 추후 수정 필요
 import React from "react";
 import InputProps from "./props/login_input.props";
 
@@ -5,7 +7,7 @@ import InputProps from "./props/login_input.props";
  * @file input.tsx
  * @brief 입력 필드 컴포넌트 파일
  * @details 이 파일은 다양한 타입의 입력 필드를 생성할 수 있는 재사용 가능한 Input 컴포넌트를 정의한다.
- * 
+ *
  * @author @zoeznm
  * @date 2024-08-25
  */
@@ -33,4 +35,3 @@ const Input: React.FC<InputProps> = ({
 };
 
 export default Input;
-
