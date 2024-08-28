b74d891c979f5e0c397b2aebc9e7fd24994aba1d
naviadev <qo2oq1020@gmail.com>
2024-08-28 11:27:24 +0900

    Feat | postFetch.ts : fetch 서비스 모듈화

---
+++ client/service/postFetch.ts
@@ -0,0 +1,12 @@
+const postFetchData = async (stringfy: string, url: string) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: stringfy
+  });
+  return res;
+}
+export default postFetchData;
