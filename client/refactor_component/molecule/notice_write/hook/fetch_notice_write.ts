8b080c6675e77e5891ce6c42099d0f0b43dedee3
신지윤 <ranji12@naver.com>
2024-08-29 15:23:25 +0900

    Refactor|게시물 작성 fetch부분 molecule폴더에 작성

---
+++ client/refactor_component/molecule/notice_write/hook/fetch_notice_write.ts
@@ -0,0 +1,18 @@
+const send = (state:string, stateContent:string) => {
+  return fetch("http://localhost:3001/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ title: state, content: stateContent }),
+  })
+    .then((response) =>response.text())
+    .then((data) => {
+      console.log("Success", data);
+    })
+    .catch((error) => {
+      console.error("Server Data Error", error);
+    });
+};
+export default send;
