b35962057f48da1d09a19e0bb2e6afdcc50dded8
신지윤 <ranji12@naver.com>
2024-08-29 14:55:53 +0900

    Refactor|게시판 footer 부분 (최하위부분) 리팩토링 완료

---
+++ client/refactor_component/template/notice_main_footer/notice_main.footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WriteButton from 'client/refactor_component/molecule/notice_main_footer/notice_footer';
+
+const NoticeMainFooter = () => {
+  return (
+    <WriteButton />
+  );
+};
+
+export default NoticeMainFooter;
