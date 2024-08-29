eaf92cfa8e6b5a91b1acfa3c46bae1861dfc889c
신지윤 <ranji12@naver.com>
2024-08-29 12:55:15 +0900

    Refactor|메인게시판 user게시판 fetch  molecule폴더로 이동

---
+++ client/refactor_component/molecule/notice_main_content/hook/fetch_notcie_main.ts
@@ -0,0 +1,17 @@
+import { ListNotice } from "../interface/notice_main_interface";
+const fetchNotices = (currentPage:number, itemsPerPage:number, setUserList: React.Dispatch<React.SetStateAction<ListNotice[]>>, setTotalPages: React.Dispatch<React.SetStateAction<number>>) => {
+  return fetch(
+    `http://localhost:3001/notices?page=${currentPage}&limit=${itemsPerPage}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data: { notices: ListNotice[]; totalPages: number }) => {
+      setUserList(data.notices); // 게시물 데이터
+      setTotalPages(data.totalPages); // 총 페이지 수 설정
+    })
+    .catch((err) => {
+      console.error("데이터를 가져오는 중 오류 발생:", err);
+    });
+};
+export default fetchNotices;
