adb0fbbfe3517a9e7d63b90c02586d2c1310b02a
신지윤 <ranji12@naver.com>
2024-08-29 13:05:52 +0900

    Refactor|user게시판 리스트부분과 pages 합쳐서 생성

---
+++ client/refactor_component/organism/notice_main_user_content/notice_link_pages.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import LinkList from "client/refactor_component/organism/notice_main_user_content/notice_link";
+import Pages from "client/refactor_component/molecule/notice_main_user_content/notice_pages";
+import { ListNotice } from "client/refactor_component/molecule/notice_main_user_content/interface/notice_main_interface";
+
+interface NoticeContentProps {
+  userList: ListNotice[];
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const NoticeContent: React.FC<NoticeContentProps> = ({
+  userList,
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  return (
+    <>
+      <LinkList userList={userList} currentPage={currentPage} />
+      <Pages totalPages={totalPages} currentPage={currentPage} onPageChange={onPageChange} />
+    </>
+  );
+};
+
+export default NoticeContent;
