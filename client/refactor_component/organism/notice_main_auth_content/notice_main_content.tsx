08077dc210da1254ef6a03ea4ad84ed727808107
신지윤 <ranji12@naver.com>
2024-08-29 16:18:00 +0900

    Refactor|관리자게시물 link list리팩토링

---
+++ client/refactor_component/organism/notice_main_auth_content/notice_main_content.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import * as styles from "../../../styles/notice/notice.css";
+import LinkTag from "client/refactor_component/molecule/notice_main_admin_content/notice_admin_link_tags";
+import { NoticeAuthListProps } from "./interface/notice_list_auth_props";
+
+const NoticeAuthList: React.FC<NoticeAuthListProps> = ({ authList }) => {
+  return (
+    <div className={styles.authcontentdiv}>
+      {authList.length > 0 ? (
+        authList.map((notice, index) => (
+          <div key={notice._id}>
+            <LinkTag
+              noticeId={notice._id}
+              index={index}
+              title={notice.title}
+              userId={notice.user_id}
+              createdAt={notice.createdAt}
+            />
+          </div>
+        ))
+      ) : (
+        <div>게시물 없음</div>
+      )}
+    </div>
+  );
+};
+
+export default NoticeAuthList;
