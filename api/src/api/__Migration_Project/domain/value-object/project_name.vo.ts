e2d6e2bc169c6e93cdba2247d20e05c86c142be2
naviadev <qo2oq1020@gmail.com>
2024-08-30 11:24:41 +0900

    Feat | project_name.vo.ts : 프로젝트 이름 객체

---
+++ api/src/api/__Migration_Project/domain/value-object/project_name.vo.ts
@@ -0,0 +1,23 @@
+/**
+ * * Class : ProjectName
+ * 작성자 : @naviadev / 2024-08-13
+ * 편집자 : @naviadev / 2024-08-30
+ * Issue :
+ * @class ProjectStartDate
+ * @param value: string
+ * @description : 프로젝트 이름 vo 객체
+ */
+export class ProjectName {
+  value: string;
+
+  constructor(value: string) {
+    if (!this.isValid(value)) {
+      throw new Error('프로젝트 이름에 대한 유효성 검사가 일치하지 않음.');
+    }
+    this.value = value;
+  }
+
+  private isValid(value: string): boolean {
+    return value.length > 0 && value.length <= 255;
+  }
+}
