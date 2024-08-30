0302b43edf32248b1e17a0b14a80ea741f9614e4
dalramjwi <dalramjwi@gmail.com>
2024-08-30 17:59:00 +0900

    Refactor|주석 추가

--- api/src/database/application/pending_users.repository.ts
+++ api/src/database/application/pending_users.repository.ts
@@ -3,18 +3,42 @@ import { DatabaseService } from '../infrastructure/database.service';
 import { PendingUserDTO } from '../../../../shared/DTO/SharedDTO';
 
 @Injectable()
+/**
+ * * Class : PendingUserRepository
+ * 작성자 : @dalramjwi / 2024-08-30
+ * 편집자 : @dalramjwi / 2024-08-30
+ * Issue :
+ * @class PendingUserRepository
+ * @param private readonly dbService: DatabaseService
+ * @description PendingUserRepository 클래스는 대기 중인 사용자 데이터를 처리하기 위한 데이터베이스 상호작용 메서드를 제공하는 저장소입니다.
+ * 이 클래스는 대기 중인 사용자 데이터를 조회하고 추가하는 기능을 수행합니다.
+ */
 class PendingUserRepository {
   private readonly tableName: string = 'pending_users';
+
+  /**
+   * PendingUserRepository 클래스의 생성자입니다.
+   * @param {DatabaseService} dbService - DatabaseService 인스턴스, 데이터베이스와의 상호작용을 처리합니다.
+   */
   constructor(private readonly dbService: DatabaseService) {}
 
-  async findAll() {
+  /**
+   * 모든 대기 중인 사용자 데이터를 조회하는 메서드입니다.
+   * @returns {Promise<any[]>} 데이터베이스에서 조회한 모든 대기 중인 사용자 데이터를 반환합니다.
+   */
+  async findAll(): Promise<any[]> {
     const result = await this.dbService.query(
       `SELECT * FROM ${this.tableName}`,
     );
     return result.rows;
   }
 
-  async findOneByUser(user_id: string) {
+  /**
+   * 특정 사용자 ID로 대기 중인 사용자를 조회하는 메서드입니다.
+   * @param {string} user_id - 조회할 사용자의 ID
+   * @returns {Promise<any>} 해당 사용자 ID에 대한 대기 중인 사용자 데이터를 반환합니다.
+   */
+  async findOneByUser(user_id: string): Promise<any> {
     const result = await this.dbService.query(
       `SELECT * FROM ${this.tableName} WHERE user_id = $1`,
       [user_id],
@@ -22,7 +46,12 @@ class PendingUserRepository {
     return result.rows[0];
   }
 
-  async InsertNewUser(userData: PendingUserDTO) {
+  /**
+   * 새로운 대기 중인 사용자를 추가하는 메서드입니다.
+   * @param {PendingUserDTO} userData - 추가할 대기 중인 사용자 데이터 객체
+   * @throws {Error} 데이터베이스 오류가 발생하면 예외를 던집니다.
+   */
+  async InsertNewUser(userData: PendingUserDTO): Promise<void> {
     // user_id | username | birth_date | address | phone | email | password
 
     const { user_id, username, birth_date, address, phone, email, password } =
@@ -53,7 +82,7 @@ class PendingUserRepository {
     try {
       await this.dbService.query(text, params);
     } catch (error) {
-      console.error('executing Query Error - 쿼리문 실행 불가 . :', error);
+      console.error('executing Query Error - 쿼리문 실행 불가 :', error);
       throw new Error('Database error');
     }
   }
