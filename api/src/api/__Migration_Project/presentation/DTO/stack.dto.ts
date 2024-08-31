1252928bc30f3f9f092957690a2b3036acf11f13
naviadev <qo2oq1020@gmail.com>
2024-08-31 21:33:44 +0900

    Style | stack.dto.ts : 스택 DTO 객체 주석 추가

--- api/src/api/__Migration_Project/presentation/DTO/stack.dto.ts
+++ api/src/api/__Migration_Project/presentation/DTO/stack.dto.ts
@@ -1,3 +1,11 @@
+/**
+ * * Class : Stack
+ * 작성자 : @naviadev / 2024-08-31
+ * 편집자 : @naviadev / 2024-08-31
+ * Issue :
+ * @class Stack
+ * @description : 스택 저장 요청에 사용되는 DTO 객체
+ */
 export class Stack {
   stack_name: string;
   stack_type: string;
