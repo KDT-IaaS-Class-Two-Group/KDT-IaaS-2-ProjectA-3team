b6546ca56878d423c889329d2158546b1c63c0cc
naviadev <qo2oq1020@gmail.com>
2024-08-31 21:37:13 +0900

    Style | project.query.controller.ts : Swagger 문법 추가

--- api/src/api/__Migration_Project/presentation/interface/project.query.controller.ts
+++ api/src/api/__Migration_Project/presentation/interface/project.query.controller.ts
@@ -8,11 +8,17 @@ import {
 import { ProjectQueryHandler } from '../../application/query/handler/project_query.handler';
 import { ProjectMemeberQuery } from '../../application/query/Project_member.query';
 import { ProjectDataByIdQuery } from '../../application/query/project_data.query';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('/project')
 export class ProjectQueryController {
   constructor(private readonly queryHandler: ProjectQueryHandler) {}
 
+  @Get('list')
+  @ApiOperation({
+    summary: '프로젝트 리스트 출력 엔드포인트',
+    description: '모든 프로젝트에 대한 데이터를 반환한다.',
+  })
   async getProjectListAll() {
     try {
       const projectList = await this.queryHandler.getProjectAllExecute();
@@ -25,6 +31,12 @@ export class ProjectQueryController {
       );
     }
   }
+
+  @ApiOperation({
+    summary: '프로젝트 팀 데이터 확인 엔드포인트',
+    description:
+      '프로젝트에 할당된 팀원들의 세부 데이터를 반환하는 역할을 수행한다.',
+  })
   @Get('/getTeamMember/:id')
   async getTeamDatat(@Param('id') id: string) {
     const query = new ProjectMemeberQuery(id);
