3b8f8e518cd529a5da3e4cfa6e3c47635a247ba5
naviadev <qo2oq1020@gmail.com>
2024-08-31 21:39:47 +0900

    Refactor | project.module.ts : 프로젝트 모듈 생성

---
+++ api/src/api/__Migration_Project/project.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { DatabaseService } from 'src/database/infrastructure/database.service';
+import { QueryBuilder } from 'src/database/infrastructure/queryBuilder';
+import { ProjectCommandController } from './presentation/interface/project.command.controller';
+import { ProjectQueryController } from './presentation/interface/project.query.controller';
+import { ProjectCommandHandler } from './application/command/handler/project_command.handler';
+import { ProjectQueryHandler } from './application/query/handler/project_query.handler';
+
+@Module({
+  imports: [],
+  controllers: [ProjectCommandController, ProjectQueryController],
+  providers: [
+    QueryBuilder,
+    DatabaseService,
+    ProjectCommandHandler,
+    ProjectQueryHandler,
+  ],
+})
+export class ProjectModule {}
