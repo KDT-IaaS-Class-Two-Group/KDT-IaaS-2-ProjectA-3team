f42014310b5b591dec733f566c83ad1bc654fd42
naviadev <qo2oq1020@gmail.com>
2024-08-31 21:26:29 +0900

    Feat | project_command.handler.ts : 프로젝트 쓰기 명령을 수행하는 Command Handler 작성

---
+++ api/src/api/__Migration_Project/application/command/handler/project_command.handler.ts
@@ -0,0 +1,22 @@
+import { ProjectRepository } from 'src/api/__Migration_Project/infrastructure/repository/project.repository';
+import { CreateProjectCommand } from '../project_create.command';
+import { Project } from 'src/api/__Migration_Project/domain/entities/project.entity';
+import { Injectable } from '@nestjs/common';
+import { AddStackCommand } from '../add_stack.command';
+@Injectable()
+export class ProjectCommandHandler {
+  constructor(private readonly repository: ProjectRepository) {}
+
+  async createProjectExecute(command: CreateProjectCommand) {
+    const domain = Project.create(command);
+    return await this.repository.createProject(domain);
+  }
+
+  async addStackExecute(command: AddStackCommand) {
+    const data = await this.repository.saveProjectStack(
+      command.stack,
+      command.project_name.getValue(),
+    );
+    return data;
+  }
+}
