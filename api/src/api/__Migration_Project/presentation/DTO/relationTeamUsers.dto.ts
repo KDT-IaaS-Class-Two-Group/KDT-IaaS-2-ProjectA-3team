113697ffe09a0f1f67ad98292a85025d25ef6b1d
naviadev <qo2oq1020@gmail.com>
2024-08-31 21:34:04 +0900

    Style | relationTeamUsers.dto.ts : 관계 테이블을 정의하며 DTO의 역할을 수행하는 객체의 해설 주석 추가

--- api/src/api/__Migration_Project/presentation/DTO/relationTeamUsers.dto.ts
+++ api/src/api/__Migration_Project/presentation/DTO/relationTeamUsers.dto.ts
@@ -1,3 +1,10 @@
+/**
+ * * Class : RelationTeamUsers
+ * 작성자 : @naviadev / 2024-08-31
+ * 편집자 : @naviadev / 2024-08-31
+ * @class RelationTeamUsers
+ * @description : Team과 User 테이블의 관계 테이블인 RelationTeamUsers 의 구조를 정의.
+ */
 export class RelationTeamUsers {
   team_name: string;
   user_id: string;
