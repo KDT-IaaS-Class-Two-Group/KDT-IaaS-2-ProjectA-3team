61704ac29b6d54307a9ea2438aa26296e4bfa07f
naviadev <qo2oq1020@gmail.com>
2024-08-31 21:43:23 +0900

    Del | project : 기존 프로젝트 디렉토리 삭제

--- api/src/api/project/DTO/relation_team_project.DTO.ts
+++
@@ -1,5 +0,0 @@
-export class RelationTeamUsers {
-  team_name: string;
-  user_id: string;
-  role_name: string;
-}
--- api/src/api/project/DTO/stack.DTO.ts
+++
@@ -1,4 +0,0 @@
-export class Stack {
-  stack_name: string;
-  stack_type: string;
-}
