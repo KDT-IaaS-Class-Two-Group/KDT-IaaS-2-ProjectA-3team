4d6557ad5538eaf1341e94aa1150765968076038
naviadev <qo2oq1020@gmail.com>
2024-09-01 16:17:20 +0900

    Fix | project_query.handler.ts : import 구문 경로 수정 __Migration_project => project

--- api/src/api/project/application/query/handler/project_query.handler.ts
+++ api/src/api/project/application/query/handler/project_query.handler.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ProjectRepository } from 'src/api/__Migration_Project/infrastructure/repository/project.repository';
+import { ProjectRepository } from 'src/api/project/infrastructure/repository/project.repository';
 import { ProjectMemeberQuery } from '../Project_member.query';
 import { ProjectDataByIdQuery } from '../project_data.query';
 
