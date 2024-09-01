539c71ae0c8d04682d1691442d4458fe2abae7f3
naviadev <qo2oq1020@gmail.com>
2024-09-01 16:22:04 +0900

    Refactor | app.module.ts : 기존 team 모듈에서 새롭게 작성한 team 모듈로 imports 구문 변경

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -13,10 +13,10 @@ import { DatabaseService } from './database/infrastructure/database.service';
 import { TableService } from './database/infrastructure/table.service';
 import { AccountModule } from './api/Account/Account.module';
 
-import { TeamModule } from './api/Team/team.module';
 import { StackModule } from './api/Stack/stack.module';
 import { IssueModule } from './api/issue/issue.module';
-import { ProjectModule } from './api/__Migration_Project/project.module';
+import { ProjectModule } from './api/project/project.module';
+import { TeamModule } from './api/_team/team.module';
 
 @Module({
   imports: [
