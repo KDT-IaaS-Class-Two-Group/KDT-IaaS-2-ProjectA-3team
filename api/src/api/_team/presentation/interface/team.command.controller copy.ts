c66b72fbceb46ca5ead544e0c3f9ad4d24296fce
naviadev <qo2oq1020@gmail.com>
2024-09-01 16:24:06 +0900

    Style | team.*.controller.ts : swagger 구문을 통한 해설 추가

--- api/src/api/_team/presentation/interface/team.command.controller copy.ts
+++ api/src/api/_team/presentation/interface/team.command.controller copy.ts
@@ -3,10 +3,37 @@ import { PostTeamCommandHandler } from '../../application/command/handler/team.c
 import { TeamDTO } from '../dto/team.dto';
 import { PostTeamCommand } from '../../application/command/handler/post_team.command';
 import { Response } from 'express';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('/team')
+@ApiTags('Team Command API')
 export class TeamCommandController {
   constructor(private readonly saveTeamHandler: PostTeamCommandHandler) {}
+  @ApiOperation({
+    summary: '팀 데이터를 저장하는 엔드포인트',
+    description: '전달받은 팀 데이터를 데이터베이스에 저장하는 엔드포인트이다.',
+  })
+  @ApiBody({
+    description: '저장할 팀 데이터의 형태를 정의한다.',
+  })
+  @ApiCreatedResponse({
+    description: '성공적으로 팀 데이터를 저장한 후의 응답이다.',
+    schema: {
+      example: {
+        message: 'success',
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '권한이 없는 요청에 대한 응답이다.',
+  })
   @Post('/save')
   async saveTeam(@Body() data: TeamDTO, @Res() response: Response) {
     const command = new PostTeamCommand(
--- api/src/api/_team/presentation/interface/team.query.controller.ts
+++ api/src/api/_team/presentation/interface/team.query.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { GetTeamAllQueryHandler } from '../../application/query/handler/get_team_all.query.handler';
 import { RES_ERROR_MSG } from 'src/api/common/enum/message/error/responseErrorMessage.enum';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('/team')
+@ApiTags('Team Query API')
 export class TeamQueryController {
   constructor(
     private readonly getTeamAllQueryHandler: GetTeamAllQueryHandler,
   ) {}
 
   @Get('/all')
+  @ApiOperation({
+    summary: '모든 팀 데이터를 조회하는 엔드포인트',
+    description: '데이터베이스에서 모든 팀의 정보를 조회하는 엔드포인트.',
+  })
+  @ApiOkResponse({
+    description: '성공적으로 모든 팀 데이터를 반환하는 응답이다.',
+  })
   async getAllTeamList() {
     try {
       const data = await this.getTeamAllQueryHandler.execute();
