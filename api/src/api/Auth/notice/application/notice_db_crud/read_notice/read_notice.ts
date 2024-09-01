4e8820407d2faccea147a0e03efe15572eb4f2cc
신지윤 <ranji12@naver.com>
2024-09-01 16:15:45 +0900

    Chore|폴더명 변경

---
+++ api/src/api/Auth/notice/application/notice_db_crud/read_notice/read_notice.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { MongoQuery } from 'src/api/Auth/notice/infrastructure/database/db_query/mongo_query';
+import { PostQuery } from 'src/api/Auth/notice/infrastructure/database/db_query/postgres_query';
+
+@Injectable()
+export class NoticeRead {
+  constructor(
+    private readonly mongoQuery: MongoQuery,
+    private readonly postQuery: PostQuery,
+  ) {}
+  async noticeRead(
+    noticeDTO,
+    tableName: string,
+    page: number,
+    limit: number,
+    // postId?: string,
+  ) {
+    const mongoCollection = await this.mongoQuery.mongoConnect(
+      'notice',
+      noticeDTO,
+      tableName,
+    );
+    const notices = await mongoCollection
+      .find()
+      .sort({ _id: -1 })
+      .skip((page - 1) * limit) // 페이지 계산
+      .limit(limit) // 페이지당 항목 수
+      .toArray();
+    // 총 항목 수를 가져오기 위해 전체 항목 수 쿼리
+    const totalCount = await mongoCollection.countDocuments();
+    // 총 페이지 수 계산
+    const totalPages = Math.ceil(totalCount / limit);
+    return { notices, totalPages }; // notices와 totalPages 반환
+    // }
+  }
+
+  async noticeAuthRead(noticeDTO, tableName, limit: number) {
+    const mongoCollection = await this.mongoQuery.mongoConnect(
+      'notice',
+      noticeDTO,
+      tableName,
+    );
+    return await mongoCollection
+      .find()
+      .sort({ _id: -1 })
+      .limit(limit)
+      .toArray();
+  }
+}
