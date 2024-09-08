bc1bfac625a4041766e875c3974fb722cab66715
신지윤 <ranji12@naver.com>
2024-09-08 16:36:16 +0900

    Refactor|login enum주석 달기

--- api/src/api/Auth/login/Enum/REDIRECT_PATH.enum.ts
+++ api/src/api/Auth/login/Enum/REDIRECT_PATH.enum.ts
@@ -1,4 +1,15 @@
+/**
+ * @enum REDIRECT_PATH
+ * @brief 다른 사용자 유형을 위한 리다이렉트 경로를 정의하는 열거형.
+ */
 export enum REDIRECT_PATH {
+  /**
+   * @brief 관리자 사용자가 메인 관리자 페이지로 접근하기 위한 경로.
+   */
   ADMIN_MAIN = '/admin/home',
+
+  /**
+   * @brief 일반 사용자가 메인 사용자 페이지로 접근하기 위한 경로.
+   */
   USER_MAIN = '/user/home',
 }
--- api/src/api/Auth/login/Enum/ROLE.enum.ts
+++ api/src/api/Auth/login/Enum/ROLE.enum.ts
@@ -1,4 +1,15 @@
+/**
+ * @enum ROLE
+ * @brief 사용자 역할을 정의하는 열거형.
+ */
 export enum ROLE {
+  /**
+   * @brief 관리자 역할.
+   */
   ADMIN = 'admin',
+
+  /**
+   * @brief 일반 사용자 역할.
+   */
   USER = 'user',
 }
