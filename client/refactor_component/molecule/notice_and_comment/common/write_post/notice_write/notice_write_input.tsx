b800b61c461fde32463b13c7c174910ac12476bd
신지윤 <ranji12@naver.com>
2024-09-11 18:47:16 +0900

    Refactor|molecule write_post폴더 주석 추가

--- client/refactor_component/molecule/notice_and_comment/common/write_post/notice_write/notice_write_input.tsx
+++ client/refactor_component/molecule/notice_and_comment/common/write_post/notice_write/notice_write_input.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import Input from "client/refactor_component/atom/input/input";
 import * as styles from "client/styles/notice/notice.css";
-
+/**
+ * @component TitleInput
+ * @description
+ * 사용자가 글 제목을 입력할 수 있는 텍스트 입력 필드를 렌더링하는 React 컴포넌트입니다.
+ * `value`는 입력 필드의 현재 값을 정의하고, `onChange`는 입력 필드의 값이 변경될 때 호출되는 함수입니다.
+ * 입력 필드는 `id`와 `placeholder` 속성을 가지고 있으며, 스타일은 `inputSize` 클래스를 사용합니다.
+ * 
+ * @param {Object} props - 컴포넌트의 속성
+ * @param {string} props.value - 입력 필드의 현재 값
+ * @param {Function} props.onChange - 입력 필드 값 변경 시 호출되는 함수
+ * 
+ * @returns {JSX.Element} - 렌더링된 입력 필드 컴포넌트
+ */
 const TitleInput: React.FC<{
   value: string;
   onChange: (ele: React.ChangeEvent<HTMLInputElement>) => void;
@@ -9,12 +21,12 @@ const TitleInput: React.FC<{
   return (
     <div className={styles.checksize}>
       <Input
-        id="title"
-        type="text"
-        value={value}
-        onChange={onChange}
-        placeholder="글 제목"
-        className={styles.inputSize}
+        id="title" // 입력 필드의 id 속성
+        type="text" //입력 필드의 타입, 텍스트 입력 사용
+        value={value} // 입력 필드의 현재 값
+        onChange={onChange} // 입력 필드 값 변경 시 호출될 함수
+        placeholder="글 제목" // 입력필드에 표시될 플레이스 홀더 텍스트
+        className={styles.inputSize} // 입력 필드 스타일
       />
     </div>
   );
--- client/refactor_component/molecule/notice_and_comment/common/write_post/notice_write/notice_write_textarea.tsx
+++ client/refactor_component/molecule/notice_and_comment/common/write_post/notice_write/notice_write_textarea.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import TextArea from "client/refactor_component/atom/text_area/text_area";
 import * as styles from "client/styles/notice/notice.css";
-
+/**
+ * @component ContentTextArea
+ * @description
+ * 사용자가 글 내용을 입력할 수 있는 텍스트 에어리어를 렌더링하는 React 컴포넌트입니다.
+ * `value`는 텍스트 에어리어의 현재 값을 정의하고, `onChange`는 텍스트 에어리어의 값이 변경될 때 호출되는 함수입니다.
+ * 텍스트 에어리어는 `name`과 `id` 속성을 가지고 있으며, 스타일은 `textareaSize` 클래스를 사용합니다.
+ * 
+ * @param {Object} props - 컴포넌트의 속성
+ * @param {string} props.value - 텍스트 에어리어의 현재 값
+ * @param {Function} props.onChange - 텍스트 에어리어 값 변경 시 호출되는 함수
+ * 
+ * @returns {JSX.Element} - 렌더링된 텍스트 에어리어 컴포넌트
+ */
 const ContentTextArea: React.FC<{
   value: string;
   onChange: (ele: React.ChangeEvent<HTMLTextAreaElement>) => void;
@@ -9,12 +21,12 @@ const ContentTextArea: React.FC<{
   return (
     <div className={styles.testsize}>
       <TextArea
-        value={value}
-        onChange={onChange}
+        value={value}// 현재 값
+        onChange={onChange} // 값 변경 시 호출되는 함수
         name="content"
         id="content"
-        placeholder="글 내용"
-        className={styles.textareaSize}
+        placeholder="글 내용"// 플레이스홀더 텍스트 설정
+        className={styles.textareaSize} //스타일
       />
     </div>
   );
