b80cb21b00c66474d3a7d8b8a1d2623fc294202a
신지윤 <ranji12@naver.com>
2024-09-12 09:43:45 +0900

    Refactor|organism main_auth폴더 주석 추가

--- client/refactor_component/organism/notice_and_comment/notice_main_auth_content/notice_main_content.tsx
+++ client/refactor_component/organism/notice_and_comment/notice_main_auth_content/notice_main_content.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import * as styles from "client/styles/notice/notice.css";
 import LinkTag from "client/refactor_component/molecule/notice_and_comment/admin/one_part_post/notice_main_admin_content/notice_admin_link_tags";
 import { NoticeAuthListProps } from "./props/notice_list_auth_props";
-
+/**
+ * @component NoticeAuthList
+ * @description 관리자 공지사항 리스트를 렌더링하는 컴포넌트
+ *
+ * @param {NoticeAuthListProps} authList - 공지사항 리스트를 담고 있는 props
+ * 
+ * @returns 공지사항이 존재하면 리스트를, 존재하지 않으면 '게시물 없음'을 표시
+ */
 const NoticeAuthList: React.FC<NoticeAuthListProps> = ({ authList }) => {
   return (
     <div className={styles.authcontentdiv}>
@@ -10,11 +17,11 @@ const NoticeAuthList: React.FC<NoticeAuthListProps> = ({ authList }) => {
         authList.map((notice, index) => (
           <div key={notice._id}>
             <LinkTag
-              noticeId={notice._id}
-              index={index}
-              title={notice.title}
-              userId={notice.user_id}
-              createdAt={notice.createdAt}
+              noticeId={notice._id} // 공지사항 ID 
+              index={index} // 공지사항 번호
+              title={notice.title} // 공지사항 제목
+              userId={notice.user_id} //공지사항 작성자
+              createdAt={notice.createdAt} // 공지사항 작성일
             />
           </div>
         ))
--- client/refactor_component/organism/notice_and_comment/notice_main_auth_content/props/notice_list_auth_props.ts
+++ client/refactor_component/organism/notice_and_comment/notice_main_auth_content/props/notice_list_auth_props.ts
@@ -1,5 +1,10 @@
 import { ListNotice } from "client/refactor_component/molecule/notice_and_comment/admin/one_part_post/notice_main_admin_content/interface/notice_admin_props";
-
+/**
+ * @interface NoticeAuthListProps
+ * @description 관리자 게시판에서 사용되는 공지 리스트 속성을 정의한 인터페이스
+ * 
+ * @property {ListNotice[]} authList - 공지사항 리스트. 각 공지사항의 상세 정보는 ListNotice 타입을 따름
+ */
 export interface NoticeAuthListProps {
   authList: ListNotice[];
 }
