22c1ece90768e09f42d1ad9e4ecc096caebfa999
신지윤 <ranji12@naver.com>
2024-09-12 09:54:04 +0900

    Refactor|template admin all_post폴더 주석 추가

--- client/refactor_component/organism/notice_and_comment/notice_main_user_content/notice_link.tsx
+++ client/refactor_component/organism/notice_and_comment/notice_main_user_content/notice_link.tsx
@@ -6,7 +6,7 @@ import { ListNotice} from "client/refactor_component/molecule/notice_and_comment
 /**
  * @interface NoticeListProps
  * @description 게시물 리스트 컴포넌트에 필요한 속성 정의
- * @property {ListNotice[]} userList - 공지사항 리스트 배열
+ * @property {ListNotice[]} userList - 게시물 리스트 배열
  * @property {number} currentPage - 현재 페이지 번호
  */
 interface NoticeListProps {
@@ -16,12 +16,12 @@ interface NoticeListProps {
 
 /**
  * @function NoticeList
- * @description 전달된 공지사항 리스트를 기반으로 공지사항 항목을 각각의 링크 컴포넌트로 렌더링. 게시물이 없을 경우 "게시물 없음" 메시지를 표시
+ * @description 전달된 게시물 리스트를 기반으로 게시물 항목을 각각의 링크 컴포넌트로 렌더링. 게시물이 없을 경우 "게시물 없음" 메시지를 표시
  * 
- * @param {NoticeListProps} userList - 공지사항 객체 리스트
- * @param {NoticeListProps} currentPage - 현재 페이지 번호로, 공지사항 리스트의 인덱싱에 사용
+ * @param {NoticeListProps} userList - 게시물 객체 리스트
+ * @param {NoticeListProps} currentPage - 현재 페이지 번호로, 게시물 리스트의 인덱싱에 사용
  * 
- * @returns 공지사항 리스트를 화면에 보여주는 JSX 요소
+ * @returns 게시물 리스트를 화면에 보여주는 JSX 요소
  */
 const NoticeList: React.FC<NoticeListProps> = ({ userList, currentPage }) => {
   return (
@@ -30,9 +30,9 @@ const NoticeList: React.FC<NoticeListProps> = ({ userList, currentPage }) => {
         userList.map((notice, index) => (
           <LinkTag
             key={notice._id}
-            notice={notice} // 게시물
-            index={index}
-            currentPage={currentPage}
+            notice={notice} // 게시물 객체 전달
+            index={index} // 게시물 번호
+            currentPage={currentPage} // 게시물 현재 페이지
           />
         ))
       ) : (
--- client/refactor_component/template/notice_and_comment/admin/all_post/notice_admin_all/notice_admin_all_content.tsx
+++ client/refactor_component/template/notice_and_comment/admin/all_post/notice_admin_all/notice_admin_all_content.tsx
@@ -16,41 +16,66 @@ import AdminAllNoticeTitlePtag from "client/refactor_component/molecule/notice_a
 import AdminAllNoticeContentPtag from "client/refactor_component/molecule/notice_and_comment/admin/all_post/notice_admin_all/notice_admin_content_tags";
 import AdminAllNoticePageBtn from "client/refactor_component/molecule/notice_and_comment/admin/all_post/notice_admin_all/notice_admin_btn";
 
+/**
+ * @function NoticeAuthAllContent
+ * @description 관리자의 모든 게시물을 렌더링하고 페이지네이션 기능을 제공하는 컴포넌트
+ */
 const NoticeAuthAllContent = () => {
   const [userList, setUserList] = useState<ListNotice[]>([]); // admin 서버에서 건너오는 게시물 데이터
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
   const [totalPages, setTotalPages] = useState(1); // 총 페이지 수
   const itemsPerPage = 8; // 한 페이지당 항목 수
 
+    /**
+   * @function useEffect
+   * @description 현재 페이지에 해당하는 게시물 데이터를 fetchNotices를 이용해 불러온다.
+   * @param {number} currentPage - 현재 페이지 번호
+   * @param {number} itemsPerPage - 한 페이지당 게시물 수
+   * @param {function} setUserList - 게시물 리스트를 설정하는 함수
+   * @param {function} setTotalPages - 총 페이지 수를 설정하는 함수
+   */
   useEffect(() => {
     fetchNotices(currentPage, itemsPerPage, setUserList, setTotalPages);
   }, [currentPage]);
 
+  /**
+   * @function pageChange
+   * @description 페이지네이션 버튼 클릭 시 페이지 번호를 변경하는 함수
+   * @param {number} page - 변경할 페이지 번호
+   */
   const pageChange = (page: number) => {
     setCurrentPage(page); // 페이지 변경
   };
-  // 함수 선언
+  
+    /**
+   * @function handleMenuClick
+   * @description 사이드바 메뉴 클릭 시 해당 컴포넌트로 전환
+   * @param {React.ReactNode} component - 렌더링할 컴포넌트
+   */
   const handleMenuClick = (component: React.ReactNode) => {
     setCurrentComponent(component);
   };
 
-  // 상태 훅 설정
+  // 상태 훅 설정 (현재 렌더링 할 컴포넌트를 상태로 관리)
   const [currentComponent, setCurrentComponent] = useState<React.ReactNode>(
     <AdminMainContent onclick={handleMenuClick} />
   );
 
   return (
     <div className={mainpagecontainer}>
+      {/* 사이드바 컴포넌트 */}
       <AdminSidebar onMenuItemClick={handleMenuClick} />
       <div>
+        {/* 페이지 상단의 메인 헤더 */}
         <MainHeader />
         <div className={pagemainmain}>
           <div className={pagemaincontainer}>
             <div className={pagemaintext}>관리자 게시물</div>{" "}
             <div className={styles.noticecontent}>
+              {/* 게시물 상단 제목 */}
               <AdminAllNoticeTitlePtag/>
               <div className={styles.usercontentdiv}>
-                {userList.length > 0 ? (
+                {userList.length > 0 ? ( // 게시물 있을 때 조건문
                   userList.map((notice, index) => (
                     <div key={notice._id}>
                       <Link
