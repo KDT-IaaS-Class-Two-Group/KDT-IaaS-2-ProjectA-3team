c842333d89f1374ea4d0d17f62369e139aacca8a
신지윤 <ranji12@naver.com>
2024-09-12 09:56:42 +0900

    Refactor|template admin one_post폴더 주석 추가

--- client/refactor_component/template/notice_and_comment/admin/one_part_post/notice_main_admin_content/notice_main_content.tsx
+++ client/refactor_component/template/notice_and_comment/admin/one_part_post/notice_main_admin_content/notice_main_content.tsx
@@ -3,13 +3,24 @@ import fetchNotices from "client/refactor_component/molecule/notice_and_comment/
 import { ListNotice } from "client/refactor_component/molecule/notice_and_comment/admin/one_part_post/notice_main_admin_content/interface/notice_admin_props";
 import AuthListContent from "client/refactor_component/organism/notice_and_comment/notice_main_auth_content/notice_main_content";
 
+/**
+ * @function NoticeAuthContent
+ * @description 게시물 데이터를 fetchNotices를 이용해 가져와 AuthListContent 컴포넌트에 전달
+ */
 const NoticeAuthContent = () => {
   const [authList, setAuthList] = useState<ListNotice[]>([]); // auth 서버에서 건너오는 게시물 데이터
 
+  /**
+   * @function useEffect
+   * @description 컴포넌트가 처음 마운트될 때 fetchNotices 호출하여 게시물 리스트 설정
+   */
   useEffect(() => {
     fetchNotices(setAuthList);
   }, []);
 
+  /**
+   * @returns {JSX.Element} AuthListContent 컴포넌트로 게시물 리스트를 렌더링
+   */
   return (
     <AuthListContent authList={authList} />
   );
