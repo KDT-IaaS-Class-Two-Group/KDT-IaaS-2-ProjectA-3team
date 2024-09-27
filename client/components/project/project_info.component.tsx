591fb6a1bebcd2272abf86b8fdd8a7553a84fd80
dalramjwi <dalramjwi@gmail.com>
2024-09-27 14:59:23 +0900

    backbutton 롤백 및 불필요한 경로 삭제

--- client/components/project/project_info.component.tsx
+++ client/components/project/project_info.component.tsx
@@ -7,7 +7,6 @@ import CalendarComponent from "client/refactor_component/molecule/calendar/calen
 import { useRouter } from "next/router";
 import MainHeader from "client/refactor_component/template/dash_board/dash_board";
 import ProjectInfoComponent from "client/components/project_info/project_info";
-import BackButton from "client/components/backButtonSection/backbutton";
 import { USERS_URL } from "client/ts/enum/url/USER_URL.enum";
 import { BUTTON_NAME } from "client/ts/enum/button_name/BUTTON_NAME.enum";
 import { pagemainmain } from "client/styles/team/teampage.css";
