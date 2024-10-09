7e365aef0f0302dc6bf55baaa702fa45420f813e
leejeayoung <leejeayoung875@gmail.com>
2024-10-09 22:40:10 +0900

    FIX | next.config.mjs 파일 오류 수정

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,11 +2,12 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,  // 기본적인 설정 추가
-  swcMinify: true,        // SWC를 사용한 최적화 설정
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-// 설정 파일을 빌드 중에 로그로 출력
-console.log('Next.js config:', nextConfig);
+// 먼저 플러그인 설정 함수를 생성합니다.
+const withVanillaExtract = createVanillaExtractPlugin();
 
-export default createVanillaExtractPlugin(nextConfig);
+// 생성된 플러그인 설정 함수를 nextConfig에 적용합니다.
+export default withVanillaExtract(nextConfig);
--- client/refactor_component/molecule/card_header/props/card_header.props.ts
+++ client/refactor_component/molecule/card_header/props/card_header.props.ts
@@ -1,4 +1,5 @@
 export default interface CardHeaderProps {
   title: string; // 카드 헤더의 타이틀
   onButtonClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; // 버튼 클릭 이벤트 핸들러
+  children?: React.ReactNode;
 }
