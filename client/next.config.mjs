08d925931fe10ec3ab7c8990108f5e8dff612a7b
leejeayoung <leejeayoung875@gmail.com>
2024-10-09 22:12:24 +0900

    FIX | next.config.mjs 파일 수정

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,4 +6,7 @@ const nextConfig = {
   swcMinify: true,        // SWC를 사용한 최적화 설정
 };
 
+// 설정 파일을 빌드 중에 로그로 출력
+console.log('Next.js config:', nextConfig);
+
 export default createVanillaExtractPlugin(nextConfig);
